34b7bcdb52dbe4f39a9c4b10cd3bbe96ff58d310
xmarya <marya_hy@hotmail.com>
2025-03-31 10:51:48 +0300

    extract validating new user data into a separate function

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,4 +1,4 @@
-import { catchAsync } from './../_utils/catchAsync';
+import { catchAsync } from "./../_utils/catchAsync";
 import { AppError } from "./../_utils/AppError";
 import User from "../models/userModel";
 import jwtSignature from "../_utils/generateSignature";
@@ -6,31 +6,19 @@ import tokenWithCookies from "../_utils/tokenWithCookies";
 import type { Request, Response } from "express";
 import { UserBasic } from "../_Types/User";
 import sanitisedData from "../_utils/sanitisedData";
-import crypto from "crypto"
+import crypto from "crypto";
+import validateNewUserData from "../_utils/validateNewUserData";
 
 export const credentialsSignup = catchAsync(async (request, response, next) => {
-  sanitisedData(request.body, next);
+  sanitisedData(request, next);
+  validateNewUserData(request, next);
 
-  const { email, password, passwordConfirm, username } = request.body;
-
-  if (!email || !username || !password || !passwordConfirm) return next(new AppError(400, "الرجاء تعبئة جميع الحقول"));
-
-  // STEP 1) check if the email exist or not:
-  const isEmailExist = await User.findOne({ email });
-  if (isEmailExist) return next(new AppError(400, "لا يمكن استخدام هذا البريد الإلكتروني  للتسجيل"));
-
-  // STEP 1) check if the email exist or not:
-  const isUsernameExist = await User.findOne({ username });
-  if (isUsernameExist) return next(new AppError(400, "الرجاء اختيار اسم مستخدم آخر"));
-
-  //STEP 3) check both passwords:
-  const isMatching = password === passwordConfirm;
-  if (!isMatching) return next(new AppError(400, "كلمات المرور غير متطابقة"));
+  const { email, password, username } = request.body;
 
   const newUser = await User.create({
     signMethod: "credentials",
     email,
-    credentials: { password} ,
+    credentials: { password },
     username,
   });
 
@@ -52,12 +40,12 @@ export const credentialsSignup = catchAsync(async (request, response, next) => {
 });
 
 export const credentialsLogin = catchAsync(async (request, response, next) => {
-  sanitisedData(request.body, next);
+  sanitisedData(request, next);
 
   // STEP 1) getting the provided email and password from the request body to check the email:
   const { email, credentials }: Pick<UserBasic, "credentials" | "email"> = request.body;
 
-  if(!credentials) return next(new AppError(400, "الرجاء تعبئة جميع الحقول"));
+  if (!credentials) return next(new AppError(400, "الرجاء تعبئة جميع الحقول"));
 
   const user = await User.findOne({ email }).select("credentials");
 
@@ -97,13 +85,13 @@ export const createDiscordUser = catchAsync(async (request, response, next) => {
 export const changePassword = catchAsync(async (request, response, next) => {
   console.log("changePassword");
 
-  sanitisedData(request.body, next);
+  sanitisedData(request, next);
   const userId = request.params.id || request.body.userId;
   const { currentPassword, confirmCurrentPassword, newPassword } = request.body;
 
   //STEP 1) check no missing input:
   if (!currentPassword || !confirmCurrentPassword || !newPassword) return next(new AppError(400, "الرجاء تعبئة جميع الحقول"));
-  
+
   //STEP 2) do they match ?
   if (currentPassword !== confirmCurrentPassword) return next(new AppError(400, "كلمات المرور غير متطابقة"));
   const user = await User.findById(userId).select("credentials"); // for testing purposes
@@ -111,11 +99,10 @@ export const changePassword = catchAsync(async (request, response, next) => {
     // const user = await User.findById(request.user.id).select("credentials");  this info is provided by protect() md
   */
 
-    if(!user) return next(new AppError(404, "هذا المستخدم غير موجود"));
+  if (!user) return next(new AppError(404, "هذا المستخدم غير موجود"));
 
   //STEP 3) is the provided password matching our record?
-  if(!await user.comparePasswords(currentPassword, user.credentials!.password))
-    return next(new AppError(401, "الرجاء التحقق من البيانات المدخلة"));
+  if (!(await user.comparePasswords(currentPassword, user.credentials!.password))) return next(new AppError(401, "الرجاء التحقق من البيانات المدخلة"));
 
   //STEP 4) allow changing the password:
   user.credentials!.password = newPassword;
@@ -123,70 +110,65 @@ export const changePassword = catchAsync(async (request, response, next) => {
 
   // STEP 5) generate a new token:
   const token = jwtSignature(userId);
-  tokenWithCookies(response,token);
+  tokenWithCookies(response, token);
 
   response.status(201).json({
     status: "success",
   });
 });
 
-export const forgetPassword = catchAsync( async (request, response, next) => {
+export const forgetPassword = catchAsync(async (request, response, next) => {
   console.log("forgetPassword");
   //STEP1 ) get the email to geth the associated user:
-  const {email} = request.body;
-  if(!email) return next(new AppError(400, "الرجاء ادخال البريد الإلكتروني"));
+  const { email } = request.body;
+  if (!email) return next(new AppError(400, "الرجاء ادخال البريد الإلكتروني"));
 
-  const user = await User.findOne({email});
+  const user = await User.findOne({ email });
   console.log("userwithemail", email);
-  if(!user) return next(new AppError(404, "الرجاء التحقق من صحة البريد الإلكتروني"));
+  if (!user) return next(new AppError(404, "الرجاء التحقق من صحة البريد الإلكتروني"));
 
   //STEP 2) generate random token and the reset URL:
   const randomToken = await user.generateRandomToken();
-  const resetURL = `${request.protocol}://${request.get(
-    "host"
-  )}/api/v1/auth/resetPassword/${randomToken}`;
+  const resetURL = `${request.protocol}://${request.get("host")}/api/v1/auth/resetPassword/${randomToken}`;
 
   //STEP 3) return the GRT to the front-end to send it vie email using Resend:
   response.status(200).json({
-    status:"success",
+    status: "success",
     randomToken,
-    resetURL
+    resetURL,
   });
-
 });
 
 export const resetPassword = catchAsync(async (request, response, next) => {
   console.log("resetPassword");
 
   //STEP 1) get the random token and compare it with the stored on in the db:
-  const hashedToken = crypto.createHash("sha256").update(request.params.randomToken).digest("hex");;
+  const hashedToken = crypto.createHash("sha256").update(request.params.randomToken).digest("hex");
 
-  const user = await User.findOne({ 
+  const user = await User.findOne({
     "credentials.passwordResetToken": hashedToken,
-    "credentials.passwordResetExpires": {$gt: new Date()}
-   }).select("credentials");
+    "credentials.passwordResetExpires": { $gt: new Date() },
+  }).select("credentials");
 
-/* CHANGE LATER: correct the condition to test the passwordResetExpires */
-  if(!user) return next(new AppError(400, "انتهت المدة المسموحة للرابط"));
-
+  /* CHANGE LATER: correct the condition to test the passwordResetExpires */
+  if (!user) return next(new AppError(400, "انتهت المدة المسموحة للرابط"));
 
   //STEP 2) get the new password since the link is still valid:
   const newPassword = request.body.newPassword;
   const newPasswordConfirm = request.body.newPasswordConfirm;
-  if(!newPassword || !newPasswordConfirm) return next(new AppError(400, "الرجاء تعبئة جميع الحقول"))
+  if (!newPassword || !newPasswordConfirm) return next(new AppError(400, "الرجاء تعبئة جميع الحقول"));
   if (newPassword !== newPasswordConfirm) return next(new AppError(400, "كلمات المرور غير متطابقة"));
 
   user.credentials!.password = newPassword;
-  user.credentials!.passwordResetToken = undefined
+  user.credentials!.passwordResetToken = undefined;
 
   await user.save(); // still validating the new password against the schema
 
   response.status(200).json({
-    status: "success"
+    status: "success",
   });
 
   //STEP 3) redirect the user to the login page (handled by front-end)
-
 });
 
 export const logout = (request: Request, response: Response) => {
