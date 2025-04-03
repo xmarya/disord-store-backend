bafd2ca023a881ff45eb9456d109e5e1ddf310ef
xmarya <marya_hy@hotmail.com>
2025-04-03 13:15:04 +0300

    create a new assistant, assistant login, checkAssistantPermission middlewares were tested

--- _routers/auth/storeRoutes.ts
+++ _routers/auth/storeRoutes.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { createStoreController, deleteStoreController, getMyStoreController, updateStoreController } from "../../controllers/auth/storeControllers";
-import { checkPermissions, restrict } from "../../controllers/auth/authController";
+import { checkAssistantPermissions, hasAuthorization, restrict } from "../../controllers/auth/authController";
 export const router = express.Router();
 
+router.route("/").post(restrict("user"), createStoreController);
+router.route("/:id").patch(restrict("storeOwner"), hasAuthorization, updateStoreController).delete(restrict("storeOwner"), hasAuthorization, deleteStoreController);
 
-router.route("/").post(restrict("user"),createStoreController);
-router.route("/:id").patch(restrict("storeOwner"), updateStoreController).delete(restrict("storeOwner"), deleteStoreController);
-
-// router.use(restrict("storeOwner", "storeAssistant"), checkPermissions("previewStoreStats")); this doesn't have access to the /:id params
-router.route("/:id").get(restrict("storeOwner", "storeAssistant"), checkPermissions("previewStoreStats"), getMyStoreController);
+// router.use(restrict("storeOwner", "storeAssistant"), checkAssistantPermissions("previewStoreStats")); this doesn't have access to the /:id params
+router.route("/:id").get(restrict("storeOwner", "storeAssistant"), hasAuthorization, checkAssistantPermissions("previewStoreStats"), getMyStoreController);
