1176a1cbe946253f82890d344b7e326ef35be390
xmarya <marya_hy@hotmail.com>
2025-04-03 15:40:02 +0300

    some changes to the controllers logic

--- controllers/auth/productController.ts
+++ controllers/auth/productController.ts
@@ -1,10 +1,35 @@
-import { createOne, getAll, getOne } from "../global";
-
-
-export const getAllProducts = getAll("Product"); // of a store
-export const getProduct = getOne("Product");
+import { AppError } from "../../_utils/AppError";
+import { catchAsync } from "../../_utils/catchAsync";
+import sanitisedData from "../../_utils/sanitisedData";
+import Product from "../../models/productModel";
+import { deleteOne, getOne, updateOne } from "../global";
 
 // protected routes
-export const createProduct = createOne("Product"); //NOTE: needs storeId
-export const updateProduct = getOne("Product");
-export const deleteProduct = getOne("Product");
+export const createProduct = catchAsync(async (request, response, next) => {
+    sanitisedData(request, next);
+
+    const newProd = await Product.create({ 
+        ...request.body,
+        store: request.params.storeId
+     });
+
+    response.status(201).json({
+      status: "success",
+      newProd,
+    });
+});
+
+export const getAllProducts = catchAsync(async (request, response, next) => {
+    const products = await Product.find({store: request.params.storeId});
+
+    if(!products) return next(new AppError(400, "لا يوجد منتجات في هذا المتجر"));
+
+    response.status(200).json({
+        status: "success",
+        products
+    });
+});
+
+export const getProduct = getOne("Product");
+export const updateProduct = updateOne("Product");
+export const deleteProduct = deleteOne("Product");
