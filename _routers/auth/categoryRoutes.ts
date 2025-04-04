aa3f2b262a46b63b239edd5429258888bf8ea0b3
xmarya <marya_hy@hotmail.com>
2025-04-04 10:09:02 +0300

    finished the category middlewares stack

--- _routers/auth/categoryRoutes.ts
+++ _routers/auth/categoryRoutes.ts
@@ -1,3 +1,13 @@
-import { Router } from "express";
+import express from "express";
+import { checkAssistantPermissions, hasAuthorization, restrict } from "../../controllers/auth/authController";
+import { createCategoryController, deleteCategoryController, getAllCategoriesController, getCategoryController, updateCategoryController } from "../../controllers/auth/categoryController";
 
-const router = Router();
+export const router = express.Router({mergeParams: true});
+
+router.use(restrict("storeOwner", "storeAssistant"), hasAuthorization);
+router.route("/").get(getAllCategoriesController);
+router.route("/:id").get(getCategoryController);
+
+router.route("/").post(checkAssistantPermissions("addCategory"), createCategoryController);
+router.route("/:id").patch(checkAssistantPermissions("editCategory"), updateCategoryController);
+router.route("/:id").delete(checkAssistantPermissions("deleteCategory"), deleteCategoryController);
