a4801abb263cb05e4708cb553bfdb9c779199e5b
xmarya <marya_hy@hotmail.com>
2025-04-06 09:10:49 +0300

    orderRouter was added to the dashboard router

--- _routers/dashboard.ts
+++ _routers/dashboard.ts
@@ -4,7 +4,9 @@ import { router as productRouter } from "./auth/productRoutes";
 import { router as assistantRouter } from "./auth/assistantRoutes";
 import { router as categoryRouter } from "./auth/categoryRoutes";
 import { router as userRouter } from "./auth/userAuthRoutes";
+import { router as orderRouter } from "./auth/orderRoutes";
 import { protect } from "../controllers/auth/authController";
+
 export const router = express.Router();
 
 console.log("dashboard routes");
@@ -13,4 +15,5 @@ router.use("/store", storeRouter);
 router.use("/:storeId/products", productRouter);
 router.use("/:storeId/categories", categoryRouter);
 router.use("/:storeId/assistants", assistantRouter);
+router.use("/orders", orderRouter);
 router.use("/me", userRouter);
