429c8a6dc523d548ca8a458660f64eeeb8429255
xmarya <marya_hy@hotmail.com>
2025-04-06 10:35:37 +0300

    no need for explicit add or get keyword in the router definition

--- _routers/auth/orderRoutes.ts
+++ _routers/auth/orderRoutes.ts
@@ -1,12 +1,13 @@
 import express from "express";
-import { AddOrder, getAllOrders } from "../../controllers/auth/orderController";
 import { generateRevenuePDF, getTotalRevenue } from "../../controllers/auth/invoiceController";
+import { AddOrder, getAllOrders } from "../../controllers/auth/orderController";
 
 export const router = express.Router();
 
-router.post("/add", AddOrder);
-router.get("/get", getAllOrders);
+router.post("/", AddOrder);
+
+router.get("/", getAllOrders);
+router.get("/invoice/pdf", generateRevenuePDF);
 
 router.get("/revenue", getTotalRevenue);
-router.get("/invoice/pdf", generateRevenuePDF);
 
