d3c8df5eecd52356bd619d8f69ba13f0357935e6
xmarya <marya_hy@hotmail.com>
2025-04-09 17:39:49 +0300

    finished the review routes stack

--- _routers/auth/reviewRoutes.ts
+++ _routers/auth/reviewRoutes.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { createReviewOnModel, getAllReviewsOnModel } from "../../controllers/auth/reviewController";
+import {createReviewController, deleteReviewController, getAllReviewsController, getOneReviewController, updateReviewController } from "../../controllers/auth/reviewController";
+import { isWriter } from "../../controllers/auth/authController";
 
 export const router = express.Router({mergeParams: true});
 
-// for stores and products
-router.route("/",).post(createReviewOnModel).get(getAllReviewsOnModel);
+// for stores and products:
+router.route("/",).post(createReviewController).get(getAllReviewsController);
 // router.route("/platform",).post(createPlatformReview).get(getAllPlatformReviews);
-router.route("/:id").get().patch().delete();
+router.route("/:id").get(getOneReviewController).patch(isWriter, updateReviewController).delete(isWriter, deleteReviewController);
+
+// for platform:
+router.route("/platform").post(createReviewController).get(getAllReviewsController);
+router.route("/platform/:id").get(getOneReviewController).patch(isWriter, updateReviewController).delete(isWriter, deleteReviewController);
