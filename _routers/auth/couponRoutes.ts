9ac29d864b888a45e4c6bf2bbc3bded45e5702e0
xmarya <marya_hy@hotmail.com>
2025-04-10 09:10:37 +0300

    is store exist middleware was added to some routers

--- _routers/auth/couponRoutes.ts
+++ _routers/auth/couponRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { checkAssistantPermissions, hasAuthorization, restrict } from "../../controllers/auth/authController";
+import { checkAssistantPermissions, hasAuthorization, isStoreIdExist, restrict } from "../../controllers/auth/authController";
 import { createCoupon } from "../../controllers/auth/couponController";
 
 export const router = express.Router({mergeParams: true});
+router.use(isStoreIdExist); // this middleware is used to ensure the storeId is exist before proceeding.
 
 router.use(restrict("storeOwner", "storeAssistant"), hasAuthorization);
 
