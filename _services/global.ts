805ecd74ec71e68d504ceb0794cf2602cd8666be
xmarya <marya_hy@hotmail.com>
2025-04-23 08:33:06 +0300

    some edits

--- _services/global.ts
+++ _services/global.ts
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
-export async function createDoc<T extends mongoose.Document>(Model: mongoose.Model<T>, data: any): Promise<T> {
-  console.log("inside createReview aka the future global create service");
-  const newDoc = await Model.create(data);
-  // newDoc.calculateRatingsAverage(Model); it'll be called from a post("save") hook
+export async function createDoc<T extends mongoose.Document>(Model: mongoose.Model<T>, data: any, locals?: any): Promise<T> {
+  console.log("inside createDoc");
+  /* OLD CODE (kept for reference): 
+    const newDoc = await Model.create(data);
+    // newDoc.calculateRatingsAverage(Model); it'll be called from a post("save") hook
+  */
+
+  const newDoc = new Model(data);
+  if (locals) newDoc.$locals.modelId = locals;
+
+  await newDoc.save();
   return newDoc;
 }
 
@@ -16,14 +23,19 @@ export async function getOneDoc<T extends mongoose.Document>(Model: mongoose.Mod
   const doc = await Model.findById(id);
   return doc;
 }
-export async function updateDoc<T extends mongoose.Document>(Model: mongoose.Model<T>, id: string | mongoose.Types.ObjectId, data: any): Promise<T | null> {
-  const updatedDoc = await Model.findByIdAndUpdate(id, {
-    data,
-    updatedAt: new Date(),
-  });
+export async function updateDoc<T extends mongoose.Document>(Model: mongoose.Model<T>, id: string | mongoose.Types.ObjectId, data: any, locals?: any): Promise<T | null> {
+  /* OLD CODE (kept for reference): 
+  const updatedDoc = await Model.findByIdAndUpdate(id, data).setOptions(locals);
+  */
+  const query = Model.findByIdAndUpdate(id, data, { runValidators: true });
+  console.log("update locals", locals);
+  if (locals) query.setOptions(locals); /*REQUIRES TESTING*/
+  const updatedDoc = await query;
   return updatedDoc;
 }
-export async function deleteDoc<T extends mongoose.Document>(Model: mongoose.Model<T>, id: string | mongoose.Types.ObjectId): Promise<T | null> {
-  const deletedDoc = await Model.findByIdAndDelete(id);
+export async function deleteDoc<T extends mongoose.Document>(Model: mongoose.Model<T>, id: string | mongoose.Types.ObjectId, locals?: any): Promise<T | null> {
+  const query = Model.findByIdAndDelete(id);
+  if (locals) query.setOptions(locals);
+  const deletedDoc = await query;
   return deletedDoc;
 }
