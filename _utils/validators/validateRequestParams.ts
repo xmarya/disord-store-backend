bef0a7c4fbdc0744d68b3a290dfaad7fa6dfe53d
xmarya <marya_hy@hotmail.com>
2025-04-25 14:26:37 +0300

    edit on the condition

--- _utils/validators/validateRequestParams.ts
+++ _utils/validators/validateRequestParams.ts
@@ -1,10 +1,13 @@
 import type { Request, Response, NextFunction } from "express";
 import { AppError } from "../AppError";
+import mongoose from "mongoose";
 
 const validateRequestParams = (paramsName: string) => {
   return (request: Request,response: Response, next: NextFunction) => {
+    console.log("validateRequestParams");
     const resourceId = request.params[paramsName];
     if (!resourceId) return next(new AppError(400, `request.params.${paramsName} is missing`));
+    if (!mongoose.Types.ObjectId.isValid(resourceId)) return next(new AppError(400, `${resourceId} is invalid`));
 
     next();
   };
