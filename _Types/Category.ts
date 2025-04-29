7bf67f797f4ab3e9d85f477bb5a9ae62f501b048
xmarya <marya_hy@hotmail.com>
2025-04-29 09:54:56 +0300

    small edits

--- _Types/Category.ts
+++ _Types/Category.ts
@@ -1,16 +1,16 @@
-import { Types } from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 
 export interface CategoryBasic {
-  id:string,
+  // id:string,
   name: string;
   colour: string;
   createdBy: {
     username:string,
     id:Types.ObjectId | string
   }
-  store:Types.ObjectId | string, // Types.ObjectId for the back-end operations. string for front-end operations
+  // store:Types.ObjectId | string, // Types.ObjectId for the back-end operations. string for front-end operations
   products?:Array<Types.ObjectId> | Array<string>
 };
 
-export type CategoryDocument = CategoryBasic;
+export type CategoryDocument = CategoryBasic & mongoose.Document;
--- _Types/Model.ts
+++ _Types/Model.ts
@@ -2,5 +2,5 @@
 
 export type Model = "User" | "Store" | "Product" | "Category"
 
-export type DynamicModel = "Store" | "Product" | "Order" | "Review-store" | "Review-product"| "Ranking-product" | "Invoice" | "StoreStat" | "AnnualProfit"
+export type DynamicModel = "Store" | "Product" | "Category"| "Order" | "Review-store" | "Review-product"| "Ranking-product" | "Invoice" | "StoreStat" | "AnnualProfit"
 // export type DynamicModel = "Store" | "Product" | "Order-store" | "Order-user" | "Review-store" | "Review-product"| "Invoice" | "StoreStat" | "AnnualProfit"
