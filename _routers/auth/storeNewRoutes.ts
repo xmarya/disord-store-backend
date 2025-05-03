bfea81dd5ed983bd1226f2fec8a6936eb6a0e19b
xmarya <marya_hy@hotmail.com>
2025-05-03 10:51:19 +0300

    continue: setting store status logic was completed

--- _routers/auth/storeNewRoutes.ts
+++ _routers/auth/storeNewRoutes.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { validateModelId } from "../../_utils/validators/validateModelId";
 import validateRequestParams from "../../_utils/validators/validateRequestParams";
 import { assignModelToRequest, checkAssistantPermissions, hasAuthorization, restrict } from "../../controllers/auth/authController";
-import { createStoreController, deleteStoreNewController, getMyStoreNewController, getStoreWithProductsController, updateStoreNewController } from "../../controllers/auth/storeControllers";
+import { createStoreController, deleteMyStoreNewController, getMyStoreNewController, getStoreWithProductsController, updateMyStoreNewController, updateMyStoreStatus } from "../../controllers/auth/storeControllers";
 import { router as reviewsRouter } from "./reviewRoutes";
 
 export const router = express.Router();
@@ -14,9 +14,10 @@ router.route("/:storeId").get(validateRequestParams("storeId"), getStoreWithProd
 
 router.route("/").post(restrict("user"), createStoreController);
 router.route("/")
-.patch(restrict("storeOwner"), hasAuthorization, updateStoreNewController)
-.delete(restrict("storeOwner"), hasAuthorization, deleteStoreNewController);
+.patch(restrict("storeOwner"), hasAuthorization, updateMyStoreNewController)
+.delete(restrict("storeOwner"), hasAuthorization, deleteMyStoreNewController);
 
+router.route("/status").patch(restrict("storeOwner"), hasAuthorization, updateMyStoreStatus);
 // storeOwner and storeAssistant only route:
 // router.use(restrict("storeOwner", "storeAssistant"), checkAssistantPermissions("previewStoreStats")); this doesn't have access to the /:storeId params
 router.route("/").get(restrict("storeOwner", "storeAssistant"), hasAuthorization, checkAssistantPermissions("previewStoreStats"), getMyStoreNewController);
