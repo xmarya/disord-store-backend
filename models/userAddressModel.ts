6ef31d85238f8696914b4a608a5bb5cfaad0dd98
xmarya <marya_hy@hotmail.com>
2025-05-05 11:03:17 +0300

    changes were made to the user schema to reflect the enhancements ofuser type + address schema and bank account schema were created

---
+++ models/userAddressModel.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+import { AddressDocument } from "../_Types/UserAddress";
+
+type AddressModel = mongoose.Model<AddressDocument>
+
+export const addressSchema = new Schema<AddressDocument>(
+    {
+      country: {
+        type: String,
+        required: [true, "the country field is required"],
+      },
+      province: {
+        type: String,
+        required: [true, "the province field is required"],
+      },
+      city: {
+        type: String,
+        required: [true, "the city field is required"],
+      },
+      district: {
+        type: String,
+        required: [true, "the district field is required"],
+      },
+      street: {
+        type: String,
+        required: [true, "the street field is required"],
+      },
+      nearestLandmark: {
+        type: String,
+        required: [true, "the nearestLandmark field is required"],
+      },
+      addressType: {
+        type: String,
+        required: [true, "the addressType field is required"],
+      },
+    },
+    {
+      timestamps: true,
+      strictQuery: true,
+      strict: true,
+      toJSON: { virtuals: true },
+      toObject: { virtuals: true },
+    }
+);
+
+const UserAddress = mongoose.model<AddressDocument, AddressModel>("UserAddress", addressSchema);
+
+export default UserAddress;
---
+++ models/userBankAccount.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { BankAccountDocument } from "../_Types/UserBankAccount";
+
+type BankAccountModel = mongoose.Model<BankAccountDocument>;
+const bankAccountSchema = new mongoose.Schema<BankAccountDocument>(
+  {
+    cardName: {type:String, required: [true, "the cardName field is required"]},
+    cardNumber: {type:String, required: [true, "the cardNumber field is required"]},
+    cardExpireIn: {
+      month: {type:String, required: [true, "the month field is required"]},
+      year: {type:String, required: [true, "the year field is required"]},
+    },
+  },
+  {
+    timestamps: true,
+    strictQuery: true,
+    strict: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+// TODO: encrypt all the bankAccount info
+
+const UserBankAccount = mongoose.model<BankAccountDocument, BankAccountModel>("UserBankAccount", bankAccountSchema);
+
+export default UserBankAccount;
