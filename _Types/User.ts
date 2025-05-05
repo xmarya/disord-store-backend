892bbfee2add5689d2abc9cf4d90246f9b55bc55
xmarya <marya_hy@hotmail.com>
2025-05-05 11:01:30 +0300

    enhanced the types of the user + a separate address and bank account types were created

--- _Types/User.ts
+++ _Types/User.ts
@@ -1,61 +1,74 @@
-import mongoose, { Types } from "mongoose";
+import mongoose from "mongoose";
 import { Plan } from "./Plan";
+import { Address } from "./UserAddress";
+import { BankAccount } from "./UserBankAccount";
 
-export interface Credentials {
+type Credentials = {
+  firstName:string,
+  lastName:string,
   password: string;
-  passwordConfirm?: string;
-  passwordResetToken?: string;
-  passwordResetExpires?: Date;
-  passwordChangedAt?: Date;
+  passwordConfirm: string;
+  passwordResetToken: string;
+  passwordResetExpires: Date;
+  passwordChangedAt: Date;
 }
 
-export interface Discord {
-  id: string;
+type Discord = {
+  discordId: string;
+  name:string,
+  username: string;
+  image: string;
 }
 
 export type UserTypes = "user" | "storeOwner" | "storeAssistant" | "admin"
 
 export interface UserDataBody {
   email: string;
-  credentials?: Credentials;
-  discord?: Discord;
-  username: string;
-  image: string;
+  credentials: Credentials;
+  discord: Discord;
 }
 
-export interface UserBasic extends UserDataBody {
-  id: string;
+export interface RegularUser extends UserDataBody {
   signMethod: "credentials" | "discord";
   userType: UserTypes;
+  firstName:string,
+  lastName:string,
+  phoneNumber:string,
+  image: string;
+  addresses: Array<Address>,
+  bankAccounts: Array<BankAccount>,
+  defaultAddressId: mongoose.Types.ObjectId,
+  defaultBankAccountId: mongoose.Types.ObjectId,
   createdAt: Date;
 }
 
-export interface UserPlan extends Plan {
-  subscribeStarts?: Date;
-  subscribeEnds?: Date;
+interface UserPlan extends Plan {
+  subscribeStarts: Date;
+  subscribeEnds: Date;
 }
 
-export interface UserOptionals {
-  bankAccount?:
-    {
-      cardName: string;
-      cardNumber: string;
-      cardExpireIn: {
-        month: string;
-        year: string;
-      };
-    }[];
-  subscribedPlanDetails?: UserPlan;
+export interface StoreOwner extends RegularUser {
+  myStore: mongoose.Types.ObjectId | string;
+  subscribedPlanDetails: UserPlan;
   pastSubscriptions?: 
     {
       plan: string;
       count: number;
     }[];
-  myStore?: Types.ObjectId | string;
 }
+
+// export interface UserOptionals {
+//   subscribedPlanDetails?: UserPlan;
+//   pastSubscriptions?: 
+//     {
+//       plan: string;
+//       count: number;
+//     }[];
+//   myStore?: Types.ObjectId | string;
+// }
 
 export interface UserMethods {comparePasswords: (providedPassword: string, userPassword: string) => Promise<boolean>;
   generateRandomToken: () => Promise<string>
 }
 
-export type UserDocument = UserBasic & UserOptionals & UserPlan & UserMethods & mongoose.Document;
+export type UserDocument = RegularUser & StoreOwner & UserPlan & UserMethods & mongoose.Document;
