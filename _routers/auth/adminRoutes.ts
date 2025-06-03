6158e06e601065e95f164494e2656b6979623826
xmarya <marya_hy@hotmail.com>
2025-06-03 20:21:11 +0300

    admin related logic for the other resources is completed

--- _routers/auth/adminRoutes.ts
+++ _routers/auth/adminRoutes.ts
@@ -1,52 +1,64 @@
 import express from "express";
 import { deleteStore, getAllStoresInfo, getOneStoreInfo, suspendStore } from "../../controllers/auth/admin/adminStoresController";
-import { restrict } from "../../controllers/auth/authController";
 import validateRequestParams from "../../_utils/validators/validateRequestParams";
-import { createUnlimitedUserController, deleteUsersController, getAllUsersController, getOneUserController } from "../../controllers/auth/admin/adminUsersController";
+import { createAdminController, createUnlimitedUserController, deleteUsersController, getAllUsersController, getOneUserController } from "../../controllers/auth/admin/adminUsersController";
 import { deletePlatformReviewController, getAllPlatformReviewsController } from "../../controllers/auth/reviewController";
 import { displayReviewInHomePage } from "../../controllers/auth/admin/adminReviewsController";
-import { getAllPlanController, getPlanController, getPlansStats, updatePlanController } from "../../controllers/auth/admin/adminPlansController";
+import { getAllPlanController, getPlanController, getMonthlyPlansStatsController, updatePlanController, getPlansStatsReportController } from "../../controllers/auth/admin/adminPlansController";
+import getDateQuery from "../../_utils/queryModifiers/getDateQuery";
+import restrict from "../../_utils/protectors/restrict";
+import sanitisedData from "../../_utils/validators/sanitisedData";
+import validateNewUserData from "../../_utils/validators/validateNewUserData";
+import validateUnlimitedUserData from "../../_utils/validators/validateUnlimitedUserData";
 
 export const router = express.Router();
 
-// NOTE: add protect + restrict later
+//  console.log("new-dashboard/ADMIN");
+
 router.use(restrict("admin"));
+router.post("/administrator-user", sanitisedData, validateNewUserData, createAdminController); // ✅
 /* STORES 
     1- patch route for suspend store
 */
 router.route("/stores").get(getAllStoresInfo);
-router.route("/stores/:storeId")
-.get(validateRequestParams("storeId"), getOneStoreInfo)
-.patch(validateRequestParams("storeId"), suspendStore)
-.delete(validateRequestParams("storeId"), deleteStore);
+router
+  .route("/stores/:storeId")
+  .get(validateRequestParams("storeId"), getDateQuery, getOneStoreInfo)
+  .patch(validateRequestParams("storeId"), suspendStore)
+  .delete(validateRequestParams("storeId"), deleteStore);
 
 /* USERS 
     1- get route for all users / one user
     2- create unlimited plan user
     3- delete user
 */
+router.get("/users", getAllUsersController); /*✅*/
+router
+  .route("/users/:userId")
+  .get(validateRequestParams("userId"), getOneUserController) /*✅*/
+  .delete(validateRequestParams("userId"), deleteUsersController);
 
-router.get("/users",getAllUsersController);
-router.route("/users/:userId").get(validateRequestParams("userId"), getOneUserController).delete(validateRequestParams("userId"), deleteUsersController);
-router.post("/users/unlimited-user",createUnlimitedUserController);
+router.post("/users/unlimited-user", sanitisedData, validateUnlimitedUserData, createUnlimitedUserController);/*✅*/
 
 /* PLANS 
     1- get route for plans stats ✅
     2- get - update route for a plan ✅
     3- get plans stats
 */
-router.get("/plans",getAllPlanController);
-router.route("/plans/:planId")
-.get(validateRequestParams("planId"), getPlanController)
-.patch(validateRequestParams("planId"), updatePlanController);
-router.get("/plans/stats", getPlansStats);
+router.get("/plans", getAllPlanController);
+// rule of 👍🏻: remember to keep the specific routes at the top of the routes stack, whilst keeping the general -especially that has /:params) at the bottom of the stack 
+router.get("/plans/monthly-stats", getDateQuery, getMonthlyPlansStatsController);/*✅*/ // for displaying data per month (the default is this month)
+router.get("/plans/plans-reports", getPlansStatsReportController); /*✅*/
+
+router
+  .route("/plans/:planId")
+  .get(validateRequestParams("planId"), getPlanController) // NOTE: then what? it's useful for getting a customisable plan -I mean the unlimited-
+  .patch(validateRequestParams("planId"), sanitisedData, updatePlanController);
+
 
 /* REVIEWS ✅
     1- get all reviews
     2- select reviews to display in the home page
 */
-
 router.get("/platform/reviews", getAllPlatformReviewsController);
-router.route("/platform/reviews/:reviewId")
-.patch(validateRequestParams("reviewId"), displayReviewInHomePage)
-.delete(validateRequestParams("reviewId"), deletePlatformReviewController);
+router.route("/platform/reviews/:reviewId").patch(validateRequestParams("reviewId"), displayReviewInHomePage).delete(validateRequestParams("reviewId"), deletePlatformReviewController); // TODO: create one for the store
