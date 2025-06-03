5ba022eedc250fa25f0e140996f6c7ee44286227
xmarya <marya_hy@hotmail.com>
2025-06-03 20:15:14 +0300

    new controllers for user's subscription

--- _routers/auth/userAuthRoutes.ts
+++ _routers/auth/userAuthRoutes.ts
@@ -1,11 +1,22 @@
 import express from "express";
-import { changePassword, updateUserProfile } from "../../controllers/auth/userAuthController";
-import { restrict } from "../../controllers/auth/authController";
+import { changePassword, createNewSubscribe, getProfile, getMySubscriptionsLogController, renewalSubscription, updateUserProfile } from "../../controllers/auth/userAuthController";
+import restrict from "../../_utils/protectors/restrict";
+import sanitisedData from "../../_utils/validators/sanitisedData";
 
 export const router = express.Router();
 
 // these below are only for userType for all types EXCEPT store assistant
 router.use(restrict("admin", "storeOwner", "user"));
-router.route("/updateProfile").patch(updateUserProfile);
-router.route("/changeMyPassword").patch(changePassword);
+router.use(sanitisedData);
+router.route("/").get(getProfile).patch(updateUserProfile); /*✅*/
+router.route("/changePassword").patch(changePassword); /*✅*/
+router.route("/deleteAccount").delete(); //TODO: delete account controller
+
+router.use(restrict("storeOwner"));
+router.route("/subscriptions").get(getMySubscriptionsLogController);
+
+router.patch("/new-subscribe", createNewSubscribe);  /*✅*/
+router.patch("/plan-renewal", renewalSubscription); // TODO: renewal subscription controller (check if the user selected the same plan or upgraded it)
+/* CHANGE LATER: the above two must have a controller for the payment as the first md before proceeding to the updating */
+router.patch("/plan-unsubscribe"); // TODO: unsubscription controller, maybe we should check if the subscription has been less than 15 days
 
--- controllers/auth/userAuthController.ts
+++ controllers/auth/userAuthController.ts
@@ -1,25 +1,57 @@
-import { updateDoc } from "../../_services/global";
-import { getUserByEmail } from "../../_services/user/userService";
-import { UserDocument } from "../../_Types/User";
+import { addDays } from "date-fns";
+import type { Request, Response } from "express";
+import { startSession } from "mongoose";
+import { SUBSCRIPTION_PERIOD } from "../../_data/constants";
+import { getOneDocByFindOne, getOneDocById, updateDoc } from "../../_services/global";
+import { updatePlanMonthlyStats } from "../../_services/plan/planService";
+import { StoreOwner, UserDocument } from "../../_Types/User";
 import { AppError } from "../../_utils/AppError";
+import { comparePasswords } from "../../_utils/authUtils";
 import { catchAsync } from "../../_utils/catchAsync";
-import jwtSignature from "../../_utils/generateSignature";
-import sanitisedData from "../../_utils/sanitisedData";
-import tokenWithCookies from "../../_utils/tokenWithCookies";
+import jwtSignature from "../../_utils/jwtToken/generateSignature";
+import tokenWithCookies from "../../_utils/jwtToken/tokenWithCookies";
+import Plan from "../../models/planModel";
 import User from "../../models/userModel";
+import formatSubscriptionsLogs from "../../_utils/queryModifiers/formatSubscriptionsLogs";
+
+export const getProfile = catchAsync(async (request, response, next) => {
+  const userId = request.user.id;
+  const userProfile = await getOneDocById(User, userId, {select: ["firstName", "lastName", "email", "image", "phoneNumber"]});
+
+  if(!userProfile) return next(new AppError(400, "لم يتم العثور على بيانات المستخدم"));
+
+  response.status(200).json({
+    success: true,
+    userProfile
+  });
+});
+
+export const getMySubscriptionsLogController = catchAsync(async (request, response, next) => {
+  const userId = request.user.id;
+  const userPlansLog = await getOneDocById(User, userId, {select: ["subscribedPlanDetails", "subscriptionsLog"]});
+  if(!userPlansLog) return next(new AppError(400, "لم يتم العثور على البيانات المطلوبة"));
+
+  const {subscribedPlanDetails, subscriptionsLog, planExpiresInDays} = userPlansLog as UserDocument & {planExpiresInDays:string};
+  const currentSubscription = formatSubscriptionsLogs(subscribedPlanDetails, planExpiresInDays);
+
+  response.status(200).json({
+    success: true,
+    currentSubscription,
+    subscriptionsLog
+  });
+});
 
 export const changePassword = catchAsync(async (request, response, next) => {
   console.log("changePassword");
 
-  sanitisedData(request, next);
   const userId = request.user.id || request.body.userId;
-  const { currentPassword, confirmCurrentPassword, newPassword } = request.body;
+  const { currentPassword, newPassword, confirmNewPassword } = request.body;
 
   //STEP 1) check no missing input:
-  if (!currentPassword || !confirmCurrentPassword || !newPassword) return next(new AppError(400, "الرجاء تعبئة جميع الحقول المطلوبة"));
+  if (!currentPassword?.trim() || !newPassword?.trim() || !confirmNewPassword?.trim()) return next(new AppError(400, "الرجاء تعبئة جميع الحقول المطلوبة"));
 
   //STEP 2) do they match ?
-  if (currentPassword !== confirmCurrentPassword) return next(new AppError(400, "كلمات المرور غير متطابقة"));
+  if (newPassword !== confirmNewPassword) return next(new AppError(400, "كلمات المرور غير متطابقة"));
   const user = await User.findById(userId).select("credentials"); // for testing purposes
   /* CHANGE LATER: 
     // const user = await User.findById(request.user.id).select("credentials");  this info is provided by protect() md
@@ -28,14 +60,14 @@ export const changePassword = catchAsync(async (request, response, next) => {
   if (!user) return next(new AppError(404, "هذا المستخدم غير موجود"));
 
   //STEP 3) is the provided password matching our record?
-  if (!(await user.comparePasswords(currentPassword, user.credentials!.password))) return next(new AppError(401, "الرجاء التحقق من البيانات المدخلة"));
+  if (!(await comparePasswords(currentPassword, user.credentials.password))) return next(new AppError(401, "الرجاء التحقق من البيانات المدخلة"));
 
   //STEP 4) allow changing the password:
-  user.credentials!.password = newPassword;
+  user.credentials.password = newPassword;
   await user.save();
 
   // STEP 5) generate a new token:
-  const token = jwtSignature(userId);
+  const token = jwtSignature(userId, "1h");
   tokenWithCookies(response, token);
 
   response.status(201).json({
@@ -43,18 +75,17 @@ export const changePassword = catchAsync(async (request, response, next) => {
   });
 });
 
-export const updateUserProfile = catchAsync(async (request, response, next) => {
-  sanitisedData(request, next);
-
+export const updateUserProfile = catchAsync(async (request, response, next) => { /*✅*/
   const userId = request.user.id;
-  const { email, firstName, lastName }: Partial<Pick<UserDocument, "email" | "firstName" | "lastName">> = request.body;
+  const { email, firstName, lastName }: Pick<UserDocument, "email" | "firstName" | "lastName"> = request.body;
 
   if (email) {
-    const isEmailExist = await getUserByEmail(email);
-    if (isEmailExist) return next(new AppError(400, "لا يمكن استخدام هذا البريد الإلكتروني  للتسجيل"));
+    const isEmailExist = await getOneDocByFindOne(User, { condition: { email } }); /*✅*/
+    console.log("isEmailExist", isEmailExist);
+    if (isEmailExist) return next(new AppError(400, "لا يمكن استخدام هذا البريد الإلكتروني"));
   }
 
-  if(!firstName || !lastName) return next(new AppError(400, "الرجاء اضافة الاسم الكامل"));
+  if (firstName?.trim() === "" && lastName?.trim() === "") return next(new AppError(400, "الرجاء تعبئة حقول الاسم بالكامل"));
 
   // if (username) {
   //   const isUsernameExist = await User.findOne({ username });
@@ -66,8 +97,62 @@ export const updateUserProfile = catchAsync(async (request, response, next) => {
 
   response.status(201).json({
     success: true,
-    updatedUser
+    updatedUser,
   });
 });
 
+export const createNewSubscribe = catchAsync(async (request, response, next) => { /*✅*/
+  const { planId, paidPrice } = request.body;
+  if(!planId?.trim() || !paidPrice?.trim()) return next(new AppError(400, "الرجاء ادخال تفاصيل الباقة"));
+  const plan = await getOneDocById(Plan, planId);
+  if (!plan) return next(new AppError(400, "لايوجد باقة بهذا المعرف"));
+
+  const userId = request.user.id;
+  const subscribeStarts = new Date();
+  const subscribeEnds = addDays(subscribeStarts, SUBSCRIPTION_PERIOD); // what is the format of this? is it plain JS Date object?
+  const userData: Partial<StoreOwner> = {
+    subscribedPlanDetails: {
+      planId: planId,
+      planName: plan.planName,
+      // originalPrice: plan.price.riyal,
+      subscriptionType: "new",
+      paid: true,
+      subscribeStarts,
+      subscribeEnds,
+      paidPrice
+    },
+  };
+
+  let updatedUser;
+  const session = await startSession();
+  try {
+    session.startTransaction();
+    // first update the Plan, then update the User
+    await updatePlanMonthlyStats(plan.planName, plan.price.riyal, "new", session);
+    updatedUser = await updateDoc(User, userId, userData, { session });
+
+    await session.commitTransaction();
+  } catch (error) {
+    await session.abortTransaction();
+    console.log((error as Error).message);
+    throw new AppError(500, "حدث خطأ أثناء معالجة العملية. الرجاء المحاولة مجددًا");
+  } finally {
+    await session.endSession();
+  }
+
+  response.status(201).json({
+    success: true,
+    updatedUser,
+  });
+});
+
+export const renewalSubscription = catchAsync(async (request, response, next) => {});
+
 // TODO: bank account controller, it's separate because it needs card data validation
+
+
+export function logout(request: Request, response: Response) {
+  response.clearCookie("jwt");
+  response.setHeader("Clear-Site-Data", "cookies"); // for browsers
+  response.status(200).json({ success: true, message: "you've logged-out" });
+}
