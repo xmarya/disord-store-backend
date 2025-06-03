868b52e6460666d7f9106936489c5832feafd00e
xmarya <marya_hy@hotmail.com>
2025-06-03 20:09:28 +0300

    refactored the routers + inject the new utils controllers

--- _routers/userRoutes.ts
+++ _routers/userRoutes.ts
@@ -1,11 +1,17 @@
 import express from "express";
-import { credentialsLogin, createNewUserController, forgetPassword, logout, resetPassword } from "../controllers/userController";
+import { credentialsLogin, createNewUserController, forgetPassword, resetPassword, createNewStoreOwnerController, createNewDiscordUser } from "../controllers/userController";
+import { adminLoginController } from "../controllers/auth/admin/adminUsersController";
+import sanitisedData from "../_utils/validators/sanitisedData";
+import validateNewUserData from "../_utils/validators/validateNewUserData";
 
 export const router = express.Router();
 console.log("user routes");
 
-router.post("/signup", createNewUserController);
+router.use(sanitisedData);
+router.post("/user-signup", validateNewUserData,createNewUserController);
+router.post("/storeOwner-signup", validateNewUserData,createNewStoreOwnerController);
 router.post("/login", credentialsLogin);
+router.post("/discord", createNewDiscordUser);
+router.post("/administrator-login", adminLoginController);
 router.patch("/resetPassword/:randomToken", resetPassword);
 router.post("/forgetPassword", forgetPassword);
-router.get("/logout", logout);
