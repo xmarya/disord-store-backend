41bcc196bdee704bfa7791d4cadcb2e059dc7ceb
xmarya <marya_hy@hotmail.com>
2025-06-03 20:20:55 +0300

    admin related logic for the other resources is completed

--- controllers/auth/admin/adminPlansController.ts
+++ controllers/auth/admin/adminPlansController.ts
@@ -1,49 +1,71 @@
-import { getAllDocs, getOneDoc, updateDoc } from "../../../_services/global";
-import { checkPlanName } from "../../../_services/plan/planService";
+import { getAllDocs, getOneDocById, updateDoc } from "../../../_services/global";
+import { checkPlanName, getMonthlyPlansStats, getPlansStatsReport } from "../../../_services/plan/planService";
 import { PlanDataBody } from "../../../_Types/Plan";
 import { AppError } from "../../../_utils/AppError";
 import { catchAsync } from "../../../_utils/catchAsync";
 import Plan from "../../../models/planModel";
 
 export const getAllPlanController = catchAsync(async (request, response, next) => {
-    const plans = await getAllDocs(Plan, request);
-    if(!plans) return next(new AppError(400, "no data was found"));
+  const plans = await getAllDocs(Plan, request);
+  if (!plans) return next(new AppError(400, "no data was found"));
 
-    response.status(200).json({
-        success: true,
-        plans
-    });
+  response.status(200).json({
+    success: true,
+    plans,
+  });
 });
 
 export const getPlanController = catchAsync(async (request, response, next) => {
-    const plan = await getOneDoc(Plan, request.params.planId);
+  const plan = await getOneDocById(Plan, request.params.planId);
 
-    if(!plan) return next(new AppError(400, "no plan was found with this id"));
-    response.status(200).json({
-        success: true,
-        plan
-    });
+  if (!plan) return next(new AppError(400, "no plan was found with this id"));
+  response.status(200).json({
+    success: true,
+    plan,
+  });
 });
 
 export const updatePlanController = catchAsync(async (request, response, next) => {
-    const {planId} = request.params;
-    const isUnlimited = await checkPlanName(planId);
-    if(!isUnlimited) return next(new AppError(403, "this is unlimited plan, you cant edit its data"));
+  // const { planId } = request.params;
+  // const isUnlimited = await checkPlanName(planId);
+  // if (!isUnlimited) return next(new AppError(403, "this is unlimited plan, you cant edit its data"));
 
-    //NOTE: what can be updated are: name, price, discount, quota, features
-    const {planName, price, discount, features, quota}:PlanDataBody = request.body;
-    if(!planName?.trim() && !price && !discount && !features.length && !quota)
-        return next(new AppError(400, "no data was provided to update"));
+  //NOTE: what can be updated are: name, price, discount, quota, features
+  const { planName, price, discount, features, quota }: PlanDataBody = request.body;
+  if (!planName?.trim() && !price && !discount && !features.length && !quota) return next(new AppError(400, "no data was provided to update"));
 
-    const updatedPlan = await updateDoc(Plan, request.params.planId, {});
-    if(!updatedPlan) return next(new AppError(500, "something went wrong, pleas try again"));
+  const updatedPlan = await updateDoc(Plan, request.params.planId, request.body);
+  if (!updatedPlan) return next(new AppError(500, "something went wrong, pleas try again"));
 
-    response.status(201).json({
-        success: true,
-        updatedPlan
-    });
+  response.status(201).json({
+    success: true,
+    updatedPlan,
+  });
 });
 
-export const getPlansStats = catchAsync(async (request, response, next) => {
-    // TODO: returns this/last/total stats
+export const getMonthlyPlansStatsController = catchAsync(async (request, response, next) => {
+  /*REQUIRES TESTING*/
+
+  const { dateFilter } = request.dateQuery;
+console.log("built dateFilter", dateFilter);
+  const allPlansStats = await getMonthlyPlansStats(dateFilter);
+  if (!allPlansStats.length) return next(new AppError(400, "couldn't find data for the specific date"));
+
+  response.status(200).json({
+    success: true,
+    allPlansStats,
+  });
+});
+
+export const getPlansStatsReportController = catchAsync(async (request, response, next) => {
+  /*REQUIRES TESTING*/
+  const { sortBy, sortOrder, year } = request.body;
+  const reports = await getPlansStatsReport(sortBy, sortOrder, year);
+
+  if (!reports) return next(new AppError(500, "something went wrong, try again later"));
+
+  response.status(200).json({
+    success: true,
+    reports,
+  });
 });
