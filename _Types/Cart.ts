df25a3fca9cef8ce416de7f27e04932d47e3387b
xmarya <marya_hy@hotmail.com>
2025-06-26 11:14:50 +0300

    enhancemets made to the cart type and model + cart service function was created

--- _Types/Cart.ts
+++ _Types/Cart.ts
@@ -1,22 +1,38 @@
+import mongoose from "mongoose";
 import { MongoId } from "./MongoId";
 
-export interface Cart {
+export interface CartDataBody {
   user: MongoId;
-  productsList: [
-    {
+  productsPerStore: Array<{
+    store: MongoId;
+    products: Array<{
       productId: MongoId;
       name: string;
-      price: number;
       image: string;
       quantity: number;
-      productType: "physical" | "digital"
-      discount?: number;
-      weight?:number
-    }
-  ];
-  total: number;
-  discountedPrice?:number
-  totalWeight?:number
+      productType: "physical" | "digital";
+      price: number; // per one (quantity = 1)
+      discount?: number; // if there is a discount is applied on this product (per quantity)
+      discountedPrice?: number; // this is depending on the existence of the discount
+      weight?: number;
+      taxAmount: number;
+      priceWithTax:number,
+    }>;
+    countOfStoreProducts:number, // sum of each product's quantity
+    totalBeforeDiscount: number; // sum of (price * quantity)
+    totalAfterDiscount: number; // sum of discountedPrice
+    totalWeight?: number;
+    appliedCoupon?:string
+    total: number; // sum of all products' priceWithTax
+  }>;
+  countOfCartProducts:number,
+  totalOfDiscounts:number, // grand discount (sum of all `totalAfterDiscount` from each store),
+  cartTotalWight?:number,
+  cartTotal: number; // grand total (sum of all `totalWithTax` from each store)
 }
 
-export type CartDocument = Cart;
+// export interface Cart extends CartDataBody {
+//   // cartTotal: number; // grand total (sum of all `totalWithTax` from each store)
+// }
+
+export type CartDocument = CartDataBody & mongoose.Document;
--- models/cartModel.ts
+++ models/cartModel.ts
@@ -11,42 +11,58 @@ const cartSchema = new Schema<CartDocument>(
       required: true,
       unique: true, // ensure one cart per user
     },
-    productsList: [
+    productsPerStore: [
       {
-        productId: {
+        storeId: {
           type: Schema.Types.ObjectId,
           ref: "Product",
-          required: [true, "product id is required for the cart"],
+          required: [true, "store id is required for the cart"],
         },
-        name: {
-          type: String,
-          required: [true, "product name is required for the cart"],
-        },
-        price: {
-          type: Number,
-          required: [true, "product price is required for the cart"],
-        },
-        productType: {
-          type: String,
-          enum: ["physical", "digital"],
-          required: [true, "product type is required for the cart"],
-        },
-        image: {
-          type: String,
-          required: [true, "product price is required for the cart"],
-        },
-        quantity: {
-          type: Number,
-          required: true,
-          min: 1,
-          default: 1,
-        },
-        discount: Number,
-        weight: Number,
+        products: [
+          {
+            productId: {
+              type: Schema.Types.ObjectId,
+              ref: "Product",
+              required: [true, "product id is required for the cart"],
+            },
+            name: {
+              type: String,
+              required: [true, "product name is required for the cart"],
+            },
+            price: {
+              type: Number,
+              required: [true, "product price is required for the cart"],
+            },
+            productType: {
+              type: String,
+              enum: ["physical", "digital"],
+              required: [true, "product type is required for the cart"],
+            },
+            image: {
+              type: String,
+              required: [true, "product price is required for the cart"],
+            },
+            quantity: {
+              type: Number,
+              required: true,
+              min: 1,
+              default: 1,
+            },
+            discount: Number,
+            discountedPrice: Number,
+            weight: Number,
+          },
+        ],
+        countOfStoreProducts: Number,
+        totalBeforeDiscount: Number, // sum of price only
+        totalAfterDiscount: Number, // sum of discountedPrice only
+        totalWeight: Number,
+        appliedCoupon: String,
+        total: Number, // sum of all products' priceWithTax
       },
     ],
-    discountedPrice: Number,
-    totalWeight: Number,
+    countOfCartProducts: Number,
+    totalOfDiscounts: Number, // grand discount (sum of all `totalAfterDiscount` from each store)
   },
   {
     timestamps: true,
@@ -59,16 +75,50 @@ const cartSchema = new Schema<CartDocument>(
 
 cartSchema.index({ user: 1 }, { unique: true });
 
-cartSchema.virtual("total").get(function () {
+/* OLD CODE (kept for reference): 
+// this virtual is the grand total (sum of all `totalWithTax` from each store)
+cartSchema.virtual("cartTotal").get(function () {
   // NOTE: I decided to make the total a virtual files to prevent the staleness in case a product's price has changed
   // using this approach ig going to retrieve the VF total each time the users access their carts
-  // IN ADDITION, this approach will save me time of manual recalculations ✨
-  return this.productsList.reduce((sum, prod) => {
-    if (!prod.discount) prod.discount = 0;
-    const totalPrice = (prod.price - (prod.discount ?? 0)) * prod.quantity;
-    return sum + totalPrice;
+  return this.productsPerStore.reduce((sum, store) => {
+    return sum + store.total;
   }, 0);
 });
+*/
+
+/* OLD CODE (kept for reference): 
+// this pre(save, findOne) hook is for grouping the products in the cart by the store they belong to:
+cartSchema.pre(["save","findOne"], async function(next) { //REQUIRES TESTING
+const doc:CartDocument = await this.model.findOne(this.getFilter()).select("productsList");
+if(!doc.productsList.length) return next();
+
+const productsIds = doc.productsList.map(prod => prod.productId);
+const products = await Product.find({_id: {$in: productsIds}}).select("store");
+
+const groupedByStore = products.reduce((groups, currentProduct) => {
+  const store = currentProduct.store;  //REQUIRES TESTING 
+  console.log("what is the type and the value of the store???", store);
+  
+  if(!groups[store]) groups[store] = [];
+  const {id:productId, name, image, price, productType} = currentProduct;
+  groups[store].push({
+    productId,
+    name,
+    productType,
+    image,
+    price,
+    ...(currentProduct.productType === "physical" && {weight: currentProduct.weight} ),
+    // quantity:??,
+    // discount:??
+    
+  });
+  return groups;
+}, {});
+
+
+next();
+});
+*/
 
 const Cart = model<CartDocument, CartModel>("Cart", cartSchema);
 
