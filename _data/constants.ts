ba477a063e524e94f23915d53d4154e66e918836
xmarya <marya_hy@hotmail.com>
2025-06-28 16:39:55 +0300

    related to the previous commit(3c59dbb52b664b598333f485da52e8f8c8fe1ac9)

--- _data/constants.ts
+++ _data/constants.ts
@@ -1,3 +1,5 @@
+export const ms = 60000;
+
 export const HASHING_SALT = 13;
 
 // for mongoose pagination:
@@ -13,41 +15,8 @@ export const SUPPORTED_DATE_FORMATS = [
   "yyyy/MM/dd",
 ];
 
-// export const SUPPORTED_DATE_FORMATS = {
-//   full: [
-//     "yyyy-MM-dd",
-//     "MM/dd/yyyy",
-//     "dd/MM/yyyy",
-//     "MMMM d, yyyy",
-//     "d MMMM, yyyy",
-//     "yyyy/MM/dd",
-//   ],
-//   month: [
-//     "January",
-//     "Jan",
-//     "February",
-//     "Feb",
-//     "March",
-//     "Mar",
-//     "April",
-//     "Apr",
-//     "May",
-//     "June",
-//     "Jun",
-//     "July",
-//     "Jul",
-//     "August",
-//     "Aug",
-//     "September",
-//     "Sep",
-//     "October",
-//     "Oct",
-//     "November",
-//     "Nov",
-//     "December",
-//     "Dec",
-//   ],
-// };
-
 export const SUBSCRIPTION_PERIOD = 30;
 export const PLAN_TRIAL_PERIOD = 10;
+
+export const REDIS_LONG_TTL = 300; // 5 minutes
+export const REDIS_SHORT_TTL = 90 // minute and half. used for stores' stats
