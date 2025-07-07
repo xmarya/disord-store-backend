af08cb05a1cd71aad24a94527df98645b5b5c5cf
xmarya <marya_hy@hotmail.com>
2025-07-07 11:25:16 +0300

    replaced old code

--- _utils/protectors/canWriteReview.ts
+++ _utils/protectors/canWriteReview.ts
@@ -5,7 +5,7 @@ import { catchAsync } from "../catchAsync";
 const canWriteReview = catchAsync(async (request, response, next) => {
   console.log("canWriteReview");
   const {id:userId, userType} = request.user;
-  const storeId = request.body.modelId || request.user.myStore; /*✅*/
+  const storeId = request.store;
 
   if(!await confirmAuthorization(userId, storeId) && userType !== "admin") return next();
   return next(new AppError(403, "store owner or store assistant can't write a review on their store"));
