5fe5698dbff744281ad9a5fe232d398636f3c3ee
xmarya <marya_hy@hotmail.com>
2025-07-17 09:10:20 +0300

    integration of the cache ogic with subscription updating and cancellation

--- _utils/startSubscription.ts
+++ _utils/startSubscription.ts
@@ -6,6 +6,7 @@ import { updatePlanMonthlyStats } from "../_services/plan/planService";
 import { createNewSubscription } from "../_services/user/userService";
 import { addDays } from "date-fns";
 import { PlanDocument, SubscriptionTypes } from "../_Types/Plan";
+import cacheUser from "./cacheControllers/user";
 
 export async function startSubscription(userId: MongoId, plan: PlanDocument, paidPrice: number, subscriptionType: SubscriptionTypes) {
   const { id: planId, planName } = plan;
@@ -24,17 +25,19 @@ export async function startSubscription(userId: MongoId, plan: PlanDocument, pai
     },
   };
 
-  let updatedUser;
   const session = await startSession();
   
-  const result = await session.withTransaction(async () => {
+  const updatedUser = await session.withTransaction(async () => {
     await updatePlanMonthlyStats(plan.planName, paidPrice, subscriptionType, session);
-    updatedUser = await createNewSubscription(userId, userData, session);
+    const updatedUser = await createNewSubscription(userId, userData, session);
 
     return updatedUser
   });
 
   await session.endSession();
 
-  return result;
+  updatedUser && cacheUser(updatedUser);
+  console.log("the returned user from the startSubscription", updatedUser);
+
+  return updatedUser;
 }
