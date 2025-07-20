94756cb2701577fdabdf7633fd3b95a6cb807351
xmarya <marya_hy@hotmail.com>
2025-07-20 11:00:54 +0300

    fixed a bug in update doc service function

--- _services/global.ts
+++ _services/global.ts
@@ -28,6 +28,7 @@ export async function getAllDocs<T extends mongoose.Document>(Model: mongoose.Mo
   return docs;
 }
 export async function getOneDocById<T extends mongoose.Document>(Model: mongoose.Model<T>, id: MongoId, options?:QueryOptions<T>): Promise<T | null> {
+  // console.log("getOneDocById", Model);
   const fields = options?.select ? options.select.join(" ") : "";/*✅*/
   const doc = await Model.findById(id).select(fields);
   return doc;
@@ -58,7 +59,7 @@ export async function getOneDocByFindOne<T extends mongoose.Document>(Model:mong
 
 export async function updateDoc<T extends mongoose.Document>(Model: mongoose.Model<T>, id: MongoId, data: any, options?:QueryOptions<T>): Promise<T | null> {
   const session = options?.session ?? null;
-  const updatedDoc = await Model.findOneAndUpdate({id, ...options?.condition}, data, {runValidators:true, new:true, session});
+  const updatedDoc = await Model.findOneAndUpdate({_id: id, ...options?.condition}, data, {runValidators:true, new:true, session});
 
   return updatedDoc;
 }
