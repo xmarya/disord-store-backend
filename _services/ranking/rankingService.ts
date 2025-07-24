ece7c156c1438d0e1a623e86cb220d5f15ba85f7
xmarya <marya_hy@hotmail.com>
2025-07-24 09:18:41 +0300

    fixed a bug where i was looping over an async function using the map, while it doesn't support async-await operations

--- _services/ranking/rankingService.ts
+++ _services/ranking/rankingService.ts
@@ -2,13 +2,15 @@ import mongoose from "mongoose";
 import { Model } from "../../_Types/Model";
 import Ranking from "../../models/rankingModel";
 import { MongoId } from "../../_Types/MongoId";
+import { RankingDocument } from "../../_Types/Ranking";
+import { StoreDocument } from "../../_Types/Store";
+import { ProductDocument } from "../../_Types/Product";
 
 export async function setRanking(Model: Extract<Model, "Store" | "Product">, session: mongoose.ClientSession) {
   // Model is Product-${storeId} || Store
   console.log("inside setRanking");
-  const sortedDocs = await mongoose
-    .model(Model)
-    .aggregate([
+  const sortedDocs = await mongoose.model(Model).aggregate(
+    [
       {
         $match: {
           ratingsQuantity: { $gt: 0 },
@@ -16,27 +18,41 @@ export async function setRanking(Model: Extract<Model, "Store" | "Product">, ses
         },
       },
       { $sort: { ratingsAverage: -1, ratingsQuantity: 1 } }, // If ratingsAverage is the same, sort by ratingsQuantity in ascending order
-    ])
-    .session(session); // Without that, the aggregation could run outside the transaction, especially if it's reading stale data before the rating update is committed
+    ],
+    { session }
+  ); // Without that, the aggregation could run outside the transaction, especially if it's reading stale data before the rating update is committed
 
+  const rankingBulkOps: mongoose.AnyBulkWriteOperation<RankingDocument>[] = [];
   // STEP 3) insert the new sorted documents inside the Ranking Model:
-  await Promise.all(
-    sortedDocs.map(async (doc, index) => {
-      console.log("doc.Id", doc._id);
-      Ranking.findOneAndUpdate({ resourceId: doc._id }, { $set: { rank: index + 1 } }, { upsert: true, session });
-    })
-  );
+  sortedDocs.map((doc, index) => {
+    rankingBulkOps.push({
+      updateOne: {
+        filter: { resourceId: doc._id },
+        update: { $set: { rank: index + 1, resource: Model } },
+        upsert: true,
+      },
+    });
+  });
+
+  await Ranking.bulkWrite(rankingBulkOps, { session });
+  // Ranking.findOneAndUpdate({ resourceId: doc._id }, { $set: { rank: index + 1, resource: Model } }, { new: true, upsert: true, session });
+
+  const resourceBulkOps:mongoose.AnyBulkWriteOperation<StoreDocument | ProductDocument>[] = [];
+  sortedDocs.map((doc, index) => {
+    resourceBulkOps.push({
+      updateOne: {
+        filter: { _id: doc._id },
+        update: { ranking: index + 1 },
+      },
+    });
+  });
 
   //STEP 4) re-rank the actual Model documents:
-  await Promise.all(
-    sortedDocs.map(async (doc, index) => {
-      mongoose.model(Model).findByIdAndUpdate(doc._id, { ranking: index + 1 }, { session });
-    })
-  );
+  await mongoose.model(Model).bulkWrite(resourceBulkOps, { session });
 }
 
-export async function removeRanking(resourceId:MongoId, session: mongoose.ClientSession) {
-  const deletedResource = await Ranking.findOneAndDelete({resourceId}).session(session);
-  if(!deletedResource) return;
+export async function removeRanking(resourceId: MongoId, session: mongoose.ClientSession) {
+  const deletedResource = await Ranking.findOneAndDelete({ resourceId }).session(session);
+  if (!deletedResource) return;
   setRanking(deletedResource.resource, session); // run it without blocking the main thread
 }
