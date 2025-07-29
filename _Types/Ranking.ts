612ee91dd85fe57d7957a76fae8c932d1acec66b
xmarya <marya_hy@hotmail.com>
2025-07-29 21:59:31 +0300

    final commit

--- _Types/Ranking.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Types } from "mongoose";
-
-export interface Ranking {
-  resourceId: Types.ObjectId;
-  resource: "Store" | "Product"
-  rank: number;
-}
-
-export type RankingDocument = Ranking & mongoose.Document;
