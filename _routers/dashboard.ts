9e2f292faec254b9cf06a44b010e8d5f95a8baaf
xmarya <marya_hy@hotmail.com>
2025-07-29 21:21:25 +0300

    the dashboard router was refactored

--- _routers/dashboard.ts
+++ _routers/dashboard.ts
@@ -4,10 +4,10 @@ import { router as adminRouter } from "./auth/adminRoutes";
 import { router as storeRouter } from "./auth/storeRoutes";
 import { router as assistantRouter } from "./auth/assistantRoutes";
 import { router as categoryRouter } from "./auth/categoryRoutes";
-import { router as userRouter } from "./auth/userAuthRoutes";
+import { router as meRouter } from "./auth/meRoutes";
 import { router as orderRouter } from "./auth/orderRoutes";
 import { router as couponsRouter } from "./auth/couponRoutes";
-import { router as platformReviewsRouter } from "./auth/platformReviewsRoutes";
+import { router as platformReviewsRouter } from "./auth/reviews/platformReviewsRoutes";
 import { router as productRouter } from "./auth/productRoutes";
 import { router as subscriptionsRouter } from "./auth/subscriptionsRoutes";
 import { verifyPlanSubscription } from "../_utils/validators/verifyPlanSubscription";
@@ -23,21 +23,26 @@ import { testInvoiceController } from "../controllers/auth/invoiceController";
 import refreshToken from "../_utils/jwtToken/refreshToken";
 import assignFromCacheToRequest from "../_utils/requestModifiers/assignFromCacheToRequest";
 import { sendConfirmationEmail } from "../_utils/email/sendConfirmationEmail";
+import { createReviewController } from "../controllers/auth/reviews/publicReviewController";
 
 export const router = express.Router();
 
 router.get("/logout", logout); // NOTE: keep this before the validateJwtToken and getUserFromPayload middlewares
 
 router.use(validateJwtToken, getUserFromPayload, refreshToken);
+router.use("/admin", adminRouter);
+router.use("/me", meRouter);
+router.use("/settings", settingsRouter);
 router.post("/emailConfirmation", sendConfirmationEmail);
 router.use("/subscriptions", restrict("storeOwner"), subscriptionsRouter);
-router.post("/new-store", restrict("storeOwner"), sanitisedData, createStoreController);
-router.use("/settings", settingsRouter)
-router.use("/me", userRouter);
-router.use("/admin", adminRouter);
+router.post("/newStore", restrict("storeOwner"), sanitisedData, createStoreController);
+router.route('/store/:resourceId/reviews').post(restrict("user"),createReviewController);
+router.route('/products/:resourceId/reviews').post(restrict("user"),createReviewController);
 router.use("/platform/reviews", platformReviewsRouter);
 
-router.use(assignFromCacheToRequest,assignStoreIdToRequest, assignPlanIdToRequest, verifyPlanSubscription);
+// for testing purpose:
+router.post("/test-invoices", testInvoiceController);
+router.use(assignFromCacheToRequest, assignStoreIdToRequest, assignPlanIdToRequest, verifyPlanSubscription);
 
 router.use("/store", storeRouter);
 router.use("/products", productRouter);
@@ -46,5 +51,9 @@ router.use("/assistants", assistantRouter);
 router.use("/:storeId/coupons", couponsRouter);
 router.use("/orders", orderRouter);
 
-// for testing purpose:
-router.post('/test-invoices', testInvoiceController);
+// TODOs:
+// 1- check the StoreAndPlan in the cache again ✅
+// 2- think of a way to reduce the PC's heavy relationship
+// 3- regular user's invoice and store's invoice
+// 4- public reviews route ✅
+// 5- what about reviews? ✅
