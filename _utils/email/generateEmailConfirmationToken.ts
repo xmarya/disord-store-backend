09f67973d0f78eeef4e325aa65a9752dad33dba6
xmarya <marya_hy@hotmail.com>
2025-08-07 08:00:14 +0300

    separate the logic of generating the email confirmation token into different util function for reusability

---
+++ _utils/email/generateEmailConfirmationToken.ts
@@ -0,0 +1,27 @@
+import type {Request} from "express";
+import { AdminDocument } from "../../_Types/admin/AdminUser";
+import { UserDocument } from "../../_Types/User";
+import { generateRandomToken } from "../generateRandomToken";
+import { createRedisHash } from "../redisOperations/redisHash";
+
+
+async function generateEmailConfirmationToken(user: UserDocument | AdminDocument, request:Request) {
+    
+  const randomToken = await generateRandomToken(user, "emailConfirmation");
+  // const uniqueEmailId = crypto.randomUUID();
+  // NOTE: using a unique id prevents transaction emails to be threaded in the inbox,
+  //  sending it within the request header tells that this email message is unique => https://youtu.be/FA2Axy_aKq8?si=t113reo4hNT0-jzN&t=193
+  const confirmUrl = `${request.protocol}://${request.hostname}/api/v1/auth/confirmEmail/${randomToken}`;
+
+  const key = `Email:${randomToken.slice(0, 12)}`;
+  const data = {
+    id: user.id,
+    userType: request.user.userType,
+  };
+
+  await createRedisHash(key, data, "one-hour");
+
+  return confirmUrl
+}
+
+export default generateEmailConfirmationToken;
--- _utils/email/sendConfirmationEmail.ts
+++ _utils/email/sendConfirmationEmail.ts
@@ -3,41 +3,18 @@ import Admin from "../../models/adminModel";
 import User from "../../models/userModel";
 import { AppError } from "../AppError";
 import { catchAsync } from "../catchAsync";
-import { generateRandomToken } from "../generateRandomToken";
-import { createRedisHash } from "../redisOperations/redisHash";
+import generateEmailConfirmationToken from "./generateEmailConfirmationToken";
 
 export const sendConfirmationEmail = catchAsync(async (request, response, next) => {
   const { userType } = request.user;
-  const query = ["user", "storeOwner"].includes(userType) ? getOneDocById(User, request.user.id, { select: ["credentials"] }) : getOneDocById(Admin, request.user.id, { select: ["credentials"] });
+  // const query = ["user", "storeOwner"].includes(userType) ? getOneDocById(User, request.user.id, { select: ["credentials"] }) : getOneDocById(Admin, request.user.id, { select: ["credentials"] });
+  const query = ["user", "storeOwner"].includes(userType) ? getOneDocById(User, request.user.id) : getOneDocById(Admin, request.user.id);
 
   const user = await query;
 
-  if (!user) return next(new AppError(400, "انتهت المدة المسموحة للرابط"));
+  if (!user) return next(new AppError(500, "حدثت مشكلة. الرجاء المحاولة مرة أخرى"));
 
-  const randomToken = await generateRandomToken(user, "emailConfirmation");
-  // const uniqueEmailId = crypto.randomUUID();
-  // NOTE: using a unique id prevents transaction emails to be threaded in the inbox,
-  //  sending it within the request header tells that this email message is unique => https://youtu.be/FA2Axy_aKq8?si=t113reo4hNT0-jzN&t=193
-  const confirmUrl = `${request.protocol}://${request.hostname}/api/v1/auth/confirmEmail/${randomToken}`;
-
-  /* OLD CODE (kept for reference):  I decided to send emails from the Next.js router to make the most of Resend and Components
-  // send to email
-  //   const {data, error} = await new Resend(process.env.RESEND_KEY).emails.send({
-    //     from:"",
-    //     // to: [request.user.email],
-    //     to: "shhmmanager1@gmail.com", // CHANGE LATER: to the actual user id after creating a real domain
-    //     subject:"",
-    //     html:""
-    //   });
-    */
-
-  const key = `Email:${randomToken.slice(0, 12)}`;
-  const data = {
-    id: user.id,
-    userType: request.user.userType,
-  };
-
-  await createRedisHash(key, data, "one-hour");
+  const confirmUrl = await generateEmailConfirmationToken(user, request);
 
   response.status(200).json({
     success: true,
