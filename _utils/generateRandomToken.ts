424371e58a48880ce5c3e47ca0fb081c3bb619ef
xmarya <marya_hy@hotmail.com>
2025-08-07 11:06:25 +0300

    fixed the expiration time of email confirmation token to be 1 hour like the cache ttl

--- _utils/generateRandomToken.ts
+++ _utils/generateRandomToken.ts
@@ -4,23 +4,24 @@ import { UserDocument } from "../_Types/User";
 import { AdminDocument } from "../_Types/admin/AdminUser";
 
 export async function generateRandomToken(doc: UserDocument | AdminDocument, tokenFor: "forgetPassword" | "emailConfirmation") {
+
   // STEP 1) Generate the token:
   const randomToken = crypto.randomBytes(32).toString("hex");
   const hashedToken = crypto.createHash("sha256").update(randomToken).digest("hex");
   //STEP 2) set an expiring time for the GRT:
   //   const tokenExpiresIn = new Date(Date.now() + 15 * 60 * 1000); // lasts for 15 minutes.
-  const tokenExpiresIn = addMinutes(new Date(), 15);
+  // const tokenExpiresIn = addMinutes(new Date(), 15);
 
   if (tokenFor === "forgetPassword") {
     //STEP 3) store the token after hashing/expiring time in credentials:
     doc.credentials.passwordResetToken = hashedToken;
-    doc.credentials.passwordResetExpires = tokenExpiresIn;
+    doc.credentials.passwordResetExpires = addMinutes(new Date(), 15);
   }
 
   else {
 
     doc.credentials.emailConfirmationToken = hashedToken;
-    doc.credentials.emailConfirmationExpires = tokenExpiresIn;
+    doc.credentials.emailConfirmationExpires = addMinutes(new Date(), 60);
   }
 
   //STEP 4) saving the changes:
