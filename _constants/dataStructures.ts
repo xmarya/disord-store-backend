5bed0a1c99b1026f482d6f2b3c87e5ef42b9783f
xmarya <marya_hy@hotmail.com>
2025-08-16 09:21:26 +0300

    refactored the _data folder and its files

---
+++ _constants/dataStructures.ts
@@ -0,0 +1,9 @@
+// for dates
+export const SUPPORTED_DATE_FORMATS = ["yyyy-MM-dd", "MM/dd/yyyy", "dd/MM/yyyy", "MMMM d, yyyy", "d MMMM, yyyy", "yyyy/MM/dd"];
+
+export const planTiers = {
+    "basic":1,
+    "plus":2,
+    "unlimited":3
+};
+
