04130fa0fa294e69642cee4391582ef49905e1a7
xmarya <marya_hy@hotmail.com>
2025-08-19 14:39:16 +0300

    fixed old import

--- _routers/auth/settingsRouter.ts
+++ _routers/auth/settingsRouter.ts
@@ -2,14 +2,14 @@ import express from "express";
 import restrict from "../../middlewares/protectors/restrict";
 import { validateChangePassword } from "../../middlewares/validators/validateChangePassword";
 import validateRequestParams from "../../middlewares/validators/validateRequestParams";
-import { confirmUserChangePassword, deleteUserAccountController, getUserProfile, updateUserProfile } from "../../controllers/auth/userAuthController";
+import { confirmUserChangePassword, deleteUserAccountController, getUserProfileController, updateUserProfileController } from "../../controllers/auth/userAuthController";
 import isEmailExist from "../../middlewares/protectors/isEmailExist";
 
 export const router = express.Router();
 
 // these below are only for userType for all types EXCEPT store assistant
 router.use(restrict("admin", "storeOwner", "user"));
-router.route("/").get(getUserProfile).patch(isEmailExist, updateUserProfile); /*✅*/
+router.route("/").get(getUserProfileController).patch(isEmailExist, updateUserProfileController); /*✅*/
 router.route("/changePassword").patch(validateChangePassword, confirmUserChangePassword); /*✅*/
 // router.route("/creditCard").post(validatePaymentData);
 // router.route("/creditCard").post(validatePaymentData, createNewCreditCardController).get(getAllCreditCardsController);
