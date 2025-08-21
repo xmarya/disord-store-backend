bd8ab5939f849f5e2c1d25eb5951c6840cc5f6c0
xmarya <marya_hy@hotmail.com>
2025-08-21 11:00:15 +0300

    fixed a bug express expected the max-age to be in ms, not in seconds; since i am not writing the raw header myself i have to pass the correct unit the framework expects

--- _utils/jwtToken/tokenWithCookies.ts
+++ _utils/jwtToken/tokenWithCookies.ts
@@ -4,7 +4,7 @@ export default function tokenWithCookies(response:Response, token:string) {
 // this function will make the token to be sent with requests and stored in the browser via cookies.
     const cookieOpt:CookieOptions = {
         // reference => https://stackoverflow.com/questions/74765575/why-was-max-age-introduced-for-cookies-when-we-already-had-expires
-        maxAge: 3600, // one-hour, => much accurate
+        maxAge: 60 * 60 * 1000 ,// 1 hour in ms => much accurate
         // expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000), // => error-prone
         httpOnly: true, // means the cookie can't be accessed or modified in anyway by the browser in client's machine which help us avoiding the XSS attacks. but this
         // will make us face a problem when implementing logout functionality, so to solve this without losing one of our security pieces
@@ -12,7 +12,5 @@ export default function tokenWithCookies(response:Response, token:string) {
         // and that is going to overwrite the current cookie in the browser .
         secure: true,
     }
-    // if (process.env.NODE_ENV === "production") cookieOpt.secure = true;
-    //(cockie's name, data we want to send in the cookie, opts for cookie).
     response.cookie("jwt", token, cookieOpt);
 }
