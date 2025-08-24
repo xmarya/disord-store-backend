e05de487eaae480b2674688c78298349bcf7275e
xmarya <marya_hy@hotmail.com>
2025-08-24 13:31:31 +0300

    made the error messages as constants

---
+++ _constants/primitives.ts
@@ -0,0 +1,9 @@
+export const ms = 60000;
+
+export const HASHING_SALT = 13;
+
+// for mongoose pagination:
+export const DOCS_PER_PAGE = 12;
+
+export const INTERNAL_ERROR_MESSAGE = "حدث خطأ أثناء معالجة العملية. حاول مجددًا";
+export const NOT_FOUND_ERROR_MESSAGE = "لم يتم العثور على البيانات المطلوبة";
--- _utils/extractSafeThrowableResult.ts
+++ _utils/extractSafeThrowableResult.ts
@@ -1,11 +1,12 @@
+import { NOT_FOUND_ERROR_MESSAGE } from "@constants/primitives";
 import { ResultAsync } from "neverthrow";
 
-type SafeThrowableResult<T> = { ok: true; result: T } | { ok: false; reason: "not-found"; message:"لا يوجد بيانات لعرضها"  } | { ok: false; reason: "error"; message: string };
+type SafeThrowableResult<T> = { ok: true; result: T } | { ok: false; reason: "not-found"; message:string  } | { ok: false; reason: "error"; message: string };
 
 function extractSafeThrowableResult<T>(safeResult: () => ResultAsync<T | null, Error>) {
   const result = safeResult().match<SafeThrowableResult<T>>(
     (result) => {
-      if (!result) return { ok: false, reason: "not-found", message: "لا يوجد بيانات لعرضها" };
+      if (!result) return { ok: false, reason: "not-found", message: NOT_FOUND_ERROR_MESSAGE };
       return { ok: true, result };
     },
     (error) => {
