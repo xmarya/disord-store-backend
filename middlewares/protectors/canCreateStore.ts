d5560f0666a41b7df986ff924a98e5cd972f7752
xmarya <marya_hy@hotmail.com>
2025-08-26 19:31:56 +0300

    new middleware and service for checking before createNewStore controller

---
+++ middlewares/protectors/canCreateStore.ts
@@ -0,0 +1,14 @@
+import getStoreOfOwner from "@services/storeServices/getStoreOfOwner";
+import { catchAsync } from "@utils/catchAsync";
+import returnError from "@utils/returnError";
+
+const canCreateStore = catchAsync(async (request, response, next) => {
+  const result = await getStoreOfOwner(request.user.id);
+
+  if (result.ok) return next(returnError({ reason: "forbidden", message: "يوجد متجر لهذا المستخدم" }));
+  if (!result.ok && result.reason === "error") return next(returnError(result));
+
+  next();
+});
+
+export default canCreateStore;
