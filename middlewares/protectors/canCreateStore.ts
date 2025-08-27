132fbda2ee1989435cff007fed1a5fef327f0615
xmarya <marya_hy@hotmail.com>
2025-08-27 08:18:45 +0300

    changed the get store of owner to handle both owner and assistants

--- middlewares/protectors/canCreateStore.ts
+++ middlewares/protectors/canCreateStore.ts
@@ -1,9 +1,9 @@
-import getStoreOfOwner from "@services/storeServices/getStoreOfOwner";
+import getStoreForAuthorisedUser from "@services/storeServices/getStoreForAuthorisedUser";
 import { catchAsync } from "@utils/catchAsync";
 import returnError from "@utils/returnError";
 
 const canCreateStore = catchAsync(async (request, response, next) => {
-  const result = await getStoreOfOwner(request.user.id);
+  const result = await getStoreForAuthorisedUser(request.user.id);
 
   if (result.ok) return next(returnError({ reason: "forbidden", message: "يوجد متجر لهذا المستخدم" }));
   if (!result.ok && result.reason === "error") return next(returnError(result));
