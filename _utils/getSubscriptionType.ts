1b64815681ab8927d4c3ecf2f32b4c8edbd74a5b
xmarya <marya_hy@hotmail.com>
2025-08-29 19:01:35 +0300

    changed the return type to be NonNullable

--- _utils/getSubscriptionType.ts
+++ _utils/getSubscriptionType.ts
@@ -4,14 +4,13 @@ import { MongoId } from "@Types/MongoId";
 import { SubscriptionTypes } from "@Types/Plan";
 import Plan from "@models/planModel";
 
-export async function getSubscriptionType(currentPlanId: MongoId, newPlanId: MongoId): Promise<Exclude<SubscriptionTypes, "new"> | null> {
+export async function getSubscriptionType(currentPlanId: MongoId, newPlanId: MongoId): Promise<NonNullable<Exclude<SubscriptionTypes, "new">>> {
   // 1- make both tostring to comparison:
   const toStringPlanIds = Array.from([currentPlanId, newPlanId], (planId) => planId.toString());
   if (toStringPlanIds[0] === toStringPlanIds[1]) return "renewal";
 
   const [currentPlan, newPlan] = await Promise.all([getOneDocById(Plan, currentPlanId, { select: ["planName"] }), getOneDocById(Plan, newPlanId, { select: ["planName"] })]);
-  if (!currentPlan || !newPlan) return null;
 
-  const subscriptionType = planTiers[newPlan.planName] > planTiers[currentPlan.planName] ? "upgrade" : "downgrade";
+  const subscriptionType = planTiers[newPlan!.planName] > planTiers[currentPlan!.planName] ? "upgrade" : "downgrade";
   return subscriptionType;
 }
