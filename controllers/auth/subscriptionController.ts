13b9d6459d9cebf7746955d79096b004b280fba1
xmarya <marya_hy@hotmail.com>
2025-08-30 11:09:13 +0300

    refactored getMySubscription and cancellSubscription controllers

--- controllers/auth/subscriptionController.ts
+++ controllers/auth/subscriptionController.ts
@@ -1,20 +1,10 @@
-import { addDays, isPast } from "date-fns";
-import { startSession } from "mongoose";
-import { PLAN_TRIAL_PERIOD } from "../../_constants/ttl";
-import { getOneDocById, updateDoc } from "@repositories/global";
-import { updatePlanMonthlyStats } from "@repositories/plan/planRepo";
-import { AppError } from "@utils/AppError";
 import { catchAsync } from "@utils/catchAsync";
-import { getSubscriptionType } from "@utils/getSubscriptionType";
-// import { startSubscription } from "@services/auth/usersServices/storeOwnerServices.ts/subscriptionsServices/createNewSubscriptionsLog";
-import Plan from "@models/planModel";
-import User from "@models/userModel";
-import cacheUser from "../../externals/redis/cacheControllers/user";
-import cacheStoreAndPlan from "../../externals/redis/cacheControllers/storeAndPlan";
+import cancelSubscription from "@services/auth/usersServices/storeOwnerServices.ts/subscriptionsServices/cancelSubscription";
 import createNewPlanSubscription from "@services/auth/usersServices/storeOwnerServices.ts/subscriptionsServices/createNewSubscription";
-import returnError from "@utils/returnError";
-import isErr from "@utils/isErr";
+import getStoreOwnerSubscriptionsLog from "@services/auth/usersServices/storeOwnerServices.ts/subscriptionsServices/getStoreOwnerSubscriptionsLog";
 import renewalStoreOwnerSubscription from "@services/auth/usersServices/storeOwnerServices.ts/subscriptionsServices/renewalStoreOwnerSubscription";
+import isErr from "@utils/isErr";
+import returnError from "@utils/returnError";
 
 export const createNewSubscribeController = catchAsync(async (request, response, next) => {
   const { planId, paidPrice } = request.body;
@@ -24,7 +14,7 @@ export const createNewSubscribeController = catchAsync(async (request, response,
   if (isErr(result)) return next(returnError({ reason: "bad-request", message: result.error }));
   if (!result?.ok) return next(returnError(result));
 
-  const {result :updatedStoreOwner } = result;
+  const { result: updatedStoreOwner } = result;
 
   response.status(203).json({
     success: true,
@@ -32,18 +22,35 @@ export const createNewSubscribeController = catchAsync(async (request, response,
   });
 });
 
+export const getMySubscriptionsLogController = catchAsync(async (request, response, next) => {
+  const result = await getStoreOwnerSubscriptionsLog(request.user.id);
+  if (!result.ok) return next(returnError(result));
+
+  const {
+    result: { currentSubscription, subscriptionsLog },
+  } = result;
+
+  response.status(200).json({
+    success: true,
+    data: {
+      currentSubscription,
+      subscriptionsLog,
+    },
+  });
+});
+
 export const renewalSubscriptionController = catchAsync(async (request, response, next) => {
   const { planId, paidPrice } = request.body;
-  if (!planId?.trim() || !paidPrice) return next(returnError( {reason: "bad-request", message:"الرجاء ادخال تفاصيل الباقة"}));
-  
+  if (!planId?.trim() || !paidPrice) return next(returnError({ reason: "bad-request", message: "الرجاء ادخال تفاصيل الباقة" }));
+
   const currentPlanId = request.plan;
   const storeOwnerId = request.user.id;
   const result = await renewalStoreOwnerSubscription(storeOwnerId, currentPlanId, planId, paidPrice);
 
-  if(isErr(result)) return next(returnError({reason: "not-found", message: result.error}));
-  if(!result.ok) return next(returnError(result));
+  if (isErr(result)) return next(returnError({ reason: "not-found", message: result.error }));
+  if (!result.ok) return next(returnError(result));
 
-  const {result: renewalData} = result;
+  const { result: renewalData } = result;
   response.status(203).json({
     success: true,
     data: { updatedUserSubscription: renewalData.subscribedPlanDetails },
@@ -52,39 +59,20 @@ export const renewalSubscriptionController = catchAsync(async (request, response
 
 export const cancelSubscriptionController = catchAsync(async (request, response, next) => {
   const { notes } = request.body; // TODO: for the admin log
-  const userId = request.user.id;
-  const user = await getOneDocById(User, userId, { select: ["subscribedPlanDetails"] });
-  if (!user || !user.subscribedPlanDetails) return next(new AppError(404, "User has no active subscription"));
+  const result = await cancelSubscription(request.user.id, request.plan, notes);
 
-  const { subscribedPlanDetails } = user;
+  if (isErr(result)) return next(returnError({ reason: "bad-request", message: result.error }));
+  if (!result.ok) return next(returnError(result));
 
-  const trialOver = isPast(addDays(subscribedPlanDetails.subscribeStarts, PLAN_TRIAL_PERIOD));
+  const {result: updatedStoreOwner, plan, planExpiryDate, isPlanPaid} = result;
 
-  if (trialOver) return next(new AppError(400, "the 10 days limit for cancellation is over"));
-
-  const session = await startSession();
-  const updatedUser = await session.withTransaction(async () => {
-    //TODO: add an admin log
-    await updatePlanMonthlyStats(subscribedPlanDetails.planName, subscribedPlanDetails.paidPrice, "cancellation" /*session*/); // FIX
-    const updatedUser = await updateDoc(User, userId, { $unset: { subscribedPlanDetails: "" } }, { session });
-
-    return updatedUser;
-  });
-
-  await session.endSession();
-
-  //TODO: refund the money using the User's bank account.
-
-  updatedUser && cacheUser(updatedUser);
-
-  request.planExpiryDate = new Date();
-  request.isPlanPaid = false;
-  request.plan = "";
-
-  await cacheStoreAndPlan(request.store, request.plan, request.isPlanPaid, request.planExpiryDate);
+  request.plan = plan;
+  request.isPlanPaid = isPlanPaid;
+  request.planExpiryDate = planExpiryDate;
+  
 
   response.status(200).json({
     success: true,
-    data: { updatedUser },
+    data: { updatedStoreOwner },
   });
 });
