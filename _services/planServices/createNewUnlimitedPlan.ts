034a810b44d020989ebc968717bb0abbe379c481
xmarya <marya_hy@hotmail.com>
2025-08-30 11:18:45 +0300

    comminting all deleted files

--- _services/planServices/createNewUnlimitedPlan.ts
+++
@@ -1,9 +0,0 @@
-import { createUnlimitedPlan } from "@repositories/plan/planRepo";
-import { UnlimitedPlanDataBody } from "@Types/Plan";
-import mongoose from "mongoose";
-
-async function createNewUnlimitedPlan(unlimitedPlanDetails: UnlimitedPlanDataBody, session: mongoose.ClientSession) {
-  return await createUnlimitedPlan(unlimitedPlanDetails, session);
-}
-
-export default createNewUnlimitedPlan;
