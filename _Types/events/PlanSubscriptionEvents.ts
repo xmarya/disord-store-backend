6871895d646488b646b042440ee7b1c5444d996f
xmarya <marya_hy@hotmail.com>
2025-09-06 08:22:38 +0300

    changed the type os storeOwner property

--- _Types/events/PlanSubscriptionEvents.ts
+++ _Types/events/PlanSubscriptionEvents.ts
@@ -1,12 +1,12 @@
 import { PlansNames, SubscriptionTypes } from "@Types/Schema/Plan";
 import { DomainEvent } from "./DomainEvent";
 import { MongoId } from "@Types/Schema/MongoId";
-import { StoreOwner } from "@Types/Schema/Users/StoreOwner";
+import { StoreOwnerDocument } from "@Types/Schema/Users/StoreOwner";
 
 export interface PlanSubscriptionUpdateEvent extends DomainEvent {
   type: "planSubscription.updated";
   payload: {
-    storeOwner: StoreOwner;
+    storeOwner: StoreOwnerDocument;
     planName: PlansNames;
     planId: MongoId;
     profit: number;
