13b445cc354aa8e86bc9c198596b2005d01c146c
xmarya <marya_hy@hotmail.com>
2025-09-06 08:37:27 +0300

    added the usertype in the jwtSignature payload in order to use it inside getUserFromPayload middleware + changed the TTL of temporeToken to be 5 minutes

--- _utils/jwtToken/generateSignature.ts
+++ _utils/jwtToken/generateSignature.ts
@@ -1,9 +1,10 @@
+import { UserTypes } from "@Types/Schema/Users/BasicUserTypes";
 import jwt from "jsonwebtoken";
 
 
-export default function jwtSignature(id:string, expiresIn: "1h" | "1m" ) {
+export default function jwtSignature(id:string, userType:UserTypes, expiresIn: "1h" | "5m" ) {
     if (!process.env.JWT_SALT) {
         throw new Error("JWT_SALT is not defined");
     }
-    return jwt.sign({id}, process.env.JWT_SALT, { expiresIn });
+    return jwt.sign({id, userType}, process.env.JWT_SALT, { expiresIn });
 }
