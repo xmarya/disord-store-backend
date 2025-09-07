00469fa28e3ab266abf2e68423ea1b7224ca6d11
xmarya <marya_hy@hotmail.com>
2025-09-07 14:01:48 +0300

    added restrict middleware to the login routes

--- _routers/nonAuth/publicAuthRoutes.ts
+++ _routers/nonAuth/publicAuthRoutes.ts
@@ -8,6 +8,7 @@ import { forgetPassword } from "../../middlewares/forgetPassword";
 import { getAuthenticaBalance } from "@config/authentica";
 import verifyLoginData from "../../middlewares/validators/verifyLoginData";
 import confirmUserEmail from "../../middlewares/validators/confirmUserEmail";
+import restrict from "@middlewares/protectors/restrict";
 
 export const router = express.Router();
 
@@ -16,13 +17,13 @@ router.post("/noOTPLogin", oldCredentialsLogin);
 router.use(sanitisedData);
 router.post("/user-signup", validateNewUserData, createNewUserController("user"));
 router.post("/storeOwner-signup", validateNewUserData, createNewUserController("storeOwner"));
-router.post("/login", credentialsLogin, verifyLoginData, getAuthenticaBalance, sendOTP);
+router.post("/login", credentialsLogin, verifyLoginData, restrict("user", "storeOwner", "storeAssistant"), getAuthenticaBalance, sendOTP);
 router.post("/otpVerify", verifyOTP);
 router.post("/discord", createNewDiscordUserController);
 router.patch("/confirmEmail/:randomToken", validateRequestParams("randomToken"), confirmUserEmail);
 router.patch("/resetPassword/:randomToken", validateRequestParams("randomToken"), resetPassword);
 router.post("/forgetPassword", forgetPassword);
 
-router.post("/administrator-login", credentialsLogin, verifyLoginData, getAuthenticaBalance, sendOTP);
+router.post("/administrator-login", credentialsLogin, verifyLoginData, restrict("admin"), getAuthenticaBalance, sendOTP);
 router.post("/administrator-forgetPassword", forgetPassword);
 router.route("/resetPassword/:randomToken").patch(validateRequestParams("randomToken"), resetPassword);
