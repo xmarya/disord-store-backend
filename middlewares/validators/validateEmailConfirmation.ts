582196331d60f4c50bb366a9c1ce4d9e702f680e
xmarya <marya_hy@hotmail.com>
2025-09-07 08:23:09 +0300

    used the error classes instead of AppError

--- middlewares/validators/validateEmailConfirmation.ts
+++ middlewares/validators/validateEmailConfirmation.ts
@@ -2,15 +2,18 @@ import { INTERNAL_ERROR_MESSAGE } from "@constants/primitives";
 import Credentials from "@models/credentialsModel";
 import { getOneDocByFindOne } from "@repositories/global";
 import { AppError } from "@Types/ResultTypes/errors/AppError";
+import { Failure } from "@Types/ResultTypes/errors/Failure";
+import { Forbidden } from "@Types/ResultTypes/errors/Forbidden";
 import { catchAsync } from "@utils/catchAsync";
+import returnError from "@utils/returnError";
 
 const validateEmailConfirmation = catchAsync(async (request, response, next) => {
   const email = request.user.email;
   const credentials = await getOneDocByFindOne(Credentials, {condition: {email}, select:["emailConfirmed"]});
-  if (!credentials) return next(new AppError(500, INTERNAL_ERROR_MESSAGE));
+  if (!credentials) return next(returnError(new Failure()));
 
   const { emailConfirmed } = credentials;
-  if (!emailConfirmed) return next(new AppError(403, "the email address has not been confirmed yet. Please confirm the email address and try again"));
+  if (!emailConfirmed) return next(returnError(new Forbidden("the email address has not been confirmed yet. Please confirm the email address and try again")));
 
   next();
 });
