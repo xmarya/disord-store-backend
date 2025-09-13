9d01115778c92a0bfd4810c0d8a9a55d1993c29e
xmarya <marya_hy@hotmail.com>
2025-09-13 19:09:38 +0300

    related to commit e988f4045310290c669c55eb87bd9bcd0421c49b

--- externals/redis/redisOperations/redisJson.ts
+++ externals/redis/redisOperations/redisJson.ts
@@ -1,20 +1,19 @@
 import redis from "@config/node-redis";
 
-
 export async function upsertRedisJson(key: string, path: string, data: any) {
-  return await redis.json.set(key, path, data)
+  return await redis.json.set(key, path, data);
   // return await redis.call("JSON.SET", key, path, JSON.stringify(data), "NX"); //success = ok, error = ReplyError
 }
 
 export async function getRedisJson<T>(key: string, path?: string): Promise<Array<T>> {
   const PATH = path ?? "$";
-  // path example `$.["user.deleted"].10[0].name`
-  const result = (await redis.json.get(key, {path:PATH})) /*as string;*/ // the [] with quotes are MUST for accessing a key that containes a dot
+  // path example `$.["user-deleted"].10[0].name`
+  const result = await redis.json.get(key, { path: PATH }); /*as string;*/ // the [] with quotes are MUST for accessing a key that containes a dot
 
   return result as Array<T>;
   // return JSON.parse(result) as Array<T>;
 }
 
 export async function deleteRedisJson(key: string, path: string) {
-  return await redis.json.del( key, {path});
+  return await redis.json.del(key, { path });
 }
