ff09c3bf90917d1e463636c2155a3d949035572d
xmarya <marya_hy@hotmail.com>
2025-09-14 20:05:19 +0300

    rabbitmq client was installed and set up

---
+++ _config/rabbitmq.ts
@@ -0,0 +1,24 @@
+import { Failure } from "@Types/ResultTypes/errors/Failure";
+import amqp, {ConfirmChannel} from "amqplib";
+import { ms } from "ms";
+
+const isDevelopment = process.env.NODE_ENV === "development";
+let rabbitChannel:ConfirmChannel | null = null;
+export async function initialiseRabbitMQ() {
+  const PORT = isDevelopment ? process.env.CLOUDAMQP_DEVELOPMENT_PORT : process.env.CLOUDAMQP_PRODUCTION_PORT;
+  const URL = `${process.env.CLOUDAMQP_URL}:${PORT}`
+  try {
+    // create connection and a channel using the created connection
+    const connection = await amqp.connect(URL);
+    const channel = await connection.createConfirmChannel();
+
+    rabbitChannel = channel;
+    console.log("🐇 connected");
+} catch (error) {
+    console.log("🐇🌋", error);
+    setTimeout(initialiseRabbitMQ, ms("2s"));
+    new Failure("Failed to connect to RabbitMQ");
+  }
+}
+
+export default rabbitChannel;
