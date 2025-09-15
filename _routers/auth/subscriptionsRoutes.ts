7f13caf69096b7f42627ce51c466db4b5b468dc1
xmarya <marya_hy@hotmail.com>
2025-09-15 15:54:39 +0300

    revert the changes on the routers order, const currentPlanId is now = planId since the route is now above check request assigners middlewares

--- _routers/auth/subscriptionsRoutes.ts
+++ _routers/auth/subscriptionsRoutes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import restrict from "../../middlewares/protectors/restrict";
 import sanitisedData from "../../middlewares/validators/sanitisedData";
-import { cancelSubscriptionController, /*createNewSubscribeController,*/ getMySubscriptionsLogController, renewalSubscriptionController } from "../../controllers/auth/subscriptionController";
+import { cancelSubscriptionController, createNewSubscribeController, getMySubscriptionsLogController, renewalSubscriptionController } from "../../controllers/auth/subscriptionController";
 
 export const router = express.Router();
 
@@ -9,7 +9,7 @@ router.use(restrict("storeOwner"));
 router.route("/").get(getMySubscriptionsLogController);
 
 router.use(sanitisedData);
-// router.patch("/newSubscribe", createNewSubscribeController);
+router.patch("/newSubscribe", createNewSubscribeController);
 router.patch("/renewal", renewalSubscriptionController);
 /* CHANGE LATER: the above two must have a controller for the payment as the first md before proceeding to the updating */
 router.patch("/unsubscribe", cancelSubscriptionController);
