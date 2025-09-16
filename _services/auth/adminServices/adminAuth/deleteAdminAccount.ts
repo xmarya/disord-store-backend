dd957fe306391243ebc28ed98dbaf22c1101db47
xmarya <marya_hy@hotmail.com>
2025-09-16 16:45:17 +0300

    deletedAdminAccount function integrated with outbox

--- _services/auth/adminServices/adminAuth/deleteAdminAccount.ts
+++ _services/auth/adminServices/adminAuth/deleteAdminAccount.ts
@@ -1,19 +1,26 @@
 import Admin from "@models/adminModel";
 import { deleteDoc } from "@repositories/global";
+import createOutboxRecord from "@services/_sharedServices/outboxRecordServices/createOutboxRecord";
+import { UserDeletedEvent } from "@Types/events/UserEvents";
 import { Failure } from "@Types/ResultTypes/errors/Failure";
+import { Success } from "@Types/ResultTypes/Success";
 import { MongoId } from "@Types/Schema/MongoId";
-import extractSafeThrowableResult from "@utils/extractSafeThrowableResult";
-import safeThrowable from "@utils/safeThrowable";
+import { startSession } from "mongoose";
 
 async function deleteAdminAccount(adminId: MongoId) {
+  const session = await startSession();
 
-  const safeDeleteAdmin = safeThrowable(
-    () => deleteDoc(Admin,adminId),
-    (error) => new Failure((error as Error).message)
-  );
+  const deletedAdmin = await session.withTransaction(async () => {
+    const deletedAdmin = await deleteDoc(Admin, adminId);
+    if (deletedAdmin) {
+      await createOutboxRecord<UserDeletedEvent>("user-deleted", { usersId: [deletedAdmin.id], emailsToDelete: [deletedAdmin.email] }, session);
+    }
+    return deletedAdmin;
+  });
 
-  return await extractSafeThrowableResult(() => safeDeleteAdmin);
+  if (!deletedAdmin) return new Failure();
 
+  return new Success(deletedAdmin);
 }
 
 export default deleteAdminAccount;
