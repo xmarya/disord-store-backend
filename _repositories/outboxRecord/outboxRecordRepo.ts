a4493b324a2d78926a0cfa0baacc1d5f77eb0cbd
xmarya <marya_hy@hotmail.com>
2025-09-16 20:37:32 +0300

    fixed a bug where the query wasn't awaited and because of $ne: null no document was returned

--- _repositories/outboxRecord/outboxRecordRepo.ts
+++ _repositories/outboxRecord/outboxRecordRepo.ts
@@ -17,7 +17,7 @@ export async function getAllOutboxRecords<T extends DomainEvent>(type: T["type"]
 export async function getOneOutboxRecord() {
   // NOTE: or find all ?? in I would go with find all, then the updateOutboxRecordStatus should updateMany
   // return await OutboxRecord.findOne({ type, status: "pending", sent: {$ne: null} });
-  return OutboxRecord.findOneAndUpdate({ status: "pending", sent: {$ne: null} }, {sent: new Date()});
+  return await OutboxRecord.findOneAndUpdate({ status: "pending" }, {sent: new Date()});
 }
 
 export async function updateOutboxRecordStatusToCompleted<T extends DomainEvent>(type: T["type"], recordId: MongoId) {
