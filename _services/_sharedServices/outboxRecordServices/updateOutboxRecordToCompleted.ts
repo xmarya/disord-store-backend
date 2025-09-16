4963c234eb861f0103bcd429dbfc5275a072e970
xmarya <marya_hy@hotmail.com>
2025-09-16 13:41:48 +0300

    related to the previous commit

--- _services/_sharedServices/outboxRecordServices/updateOutboxRecordToCompleted.ts
+++ _services/_sharedServices/outboxRecordServices/updateOutboxRecordToCompleted.ts
@@ -1,13 +1,14 @@
 import { updateOutboxRecordStatusToCompleted } from "@repositories/outboxRecord/outboxRecordRepo";
+import { DomainEvent } from "@Types/events/DomainEvent";
 import { Failure } from "@Types/ResultTypes/errors/Failure";
 import { MongoId } from "@Types/Schema/MongoId";
 import extractSafeThrowableResult from "@utils/extractSafeThrowableResult";
 import safeThrowable from "@utils/safeThrowable";
 
 
-async function updateOutboxRecordToCompleted(recordId:MongoId) {
+async function updateOutboxRecordToCompleted<T extends DomainEvent>(type:T["type"], recordId:MongoId) {
     const safeUpdateRecord = safeThrowable(
-        () => updateOutboxRecordStatusToCompleted(recordId),
+        () => updateOutboxRecordStatusToCompleted(type, recordId),
         (error) => new Failure((error as Error).message)
     )
 
