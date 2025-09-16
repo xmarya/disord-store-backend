5198392f448516c3afb13850dd78d187d46fe55c
xmarya <marya_hy@hotmail.com>
2025-09-16 09:29:00 +0300

    removed all observer pattern related code

--- app.ts
+++ app.ts
@@ -3,11 +3,6 @@ import initiateBullMQJobs from "@loaders/bullmqJobs";
 import expressLoader from "@loaders/express";
 import routerLoader from "@loaders/routers";
 import express from "express";
-import "@eventSubscribers/userEvents/initialiser";
-import "@eventSubscribers/productEvents/initialiser";
-import "@eventSubscribers/planEvents/planSubscriptionUpdate";
-import "eventSubscribers/queryResultsEvents/queryResultsFetched";
-import "@eventSubscribers/assistantEvents/initialiser";
 import { initialiseRabbitMQ } from "@config/rabbitmq";
 
 const app = express();
--- eventSubscribers/assistantEvents/initialiser.ts
+++
@@ -1,2 +0,0 @@
-import "./assistantCreated"
-import "./assistantUpdated"
