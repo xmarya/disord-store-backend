0406ee7a35be28cf57955e1552185bb985f521fe
xmarya <marya_hy@hotmail.com>
2025-09-17 09:19:14 +0300

    userDeletedRegister was added to registerEventConsumers

--- loaders/registerEventConsumers.ts
+++ loaders/registerEventConsumers.ts
@@ -1,10 +1,18 @@
-import novuSendWelcome from "@externals/novu/workflowTriggers/welcomeEmail";
 import userCreatedRegister from "@externals/rabbitmq/userCreated/userCreatedRegister";
+import userDeletedRegister from "@externals/rabbitmq/userDeleted/userDeletedRegister";
+import novuSendWelcome from "@externals/novu/workflowTriggers/welcomeEmail";
 import emailConfirmationTokenCache from "@externals/redis/cacheControllers/emailConfirmationTokenCache";
+import novuDeleteSubscriber from "@externals/novu/subscribers/deleteSubscriber";
+import deleteUserFromCache from "@externals/redis/cacheControllers/deleteUserFromCache";
+import deleteMultipleCredentials from "@services/auth/credentials/deleteMultipleCredentials";
+
 
 async function registerEventConsumers() {
-  userCreatedRegister({receiver: emailConfirmationTokenCache, queueName: `user-created-queue-redis`,  requeue:true});
-  userCreatedRegister({receiver: novuSendWelcome, queueName: `user-created-queue-novu`, requeue: true});
+  userCreatedRegister({ receiver: emailConfirmationTokenCache, queueName: `user-created-queue-redis`, requeue: true });
+  userCreatedRegister({ receiver: novuSendWelcome, queueName: `user-created-queue-novu`, requeue: true });
+  userDeletedRegister({ receiver: novuDeleteSubscriber, queueName: "user-deleted-queue-novu", requeue: true });
+  userDeletedRegister({ receiver: deleteUserFromCache, queueName: "user-deleted-queue-redis", requeue: false });
+  userDeletedRegister({ receiver: deleteMultipleCredentials, queueName: "user-deleted-queue-credentials-collection", requeue: true });
 }
 
 export default registerEventConsumers;
