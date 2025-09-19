18db3df854ce815f0a200a5e78b7d513e0784837
xmarya <marya_hy@hotmail.com>
2025-09-19 18:53:11 +0300

    added more types for OutboxEvents

--- _Types/events/OutboxEvents.ts
+++ _Types/events/OutboxEvents.ts
@@ -1,3 +1,4 @@
+import { ms } from "ms";
 import { DomainEvent } from "./DomainEvent";
 
 export type OutboxRecordsInfo = Record<string, Record<string, Record<string, boolean>>>;
@@ -6,11 +7,68 @@ export interface OutboxEvent extends DomainEvent {
   outboxRecordId: string;
 }
 
+export type PriorityLevelMap = {
+  "no-priority": number,
+  "background": number,
+  "low":number,
+  "normal": number,
+  "hight":number,
+  "urgent": number,
+  "critical": number
+};
+
+export type PriorityLevel = keyof PriorityLevelMap;
+
+const priority:PriorityLevelMap = {
+  "no-priority": 0,
+  "background": 1,
+  "low":3,
+  "normal": 5,
+  "hight": 7,
+  "urgent": 9,
+  "critical": 10
+}
+
+export type QueueOptions = {
+  durable?: boolean,
+  maxPriority?:PriorityLevel,
+  queueMode?: "default" | "lazy",
+  messageTtl?:number, // in milliseconds
+  maxLength?:number,
+  deadLetterExchange?:string, // If a message expires or is rejected → RabbitMQ republishes it to dead-letter-exchange with routing key user-deleted-redis.DLQ.
+  deadLetterRoutingKey?:string, // if not ser, RabbitMQ will reuse the original routing key.
+  expires?:number // for temporary queues
+
+}
+
+export const QUEUE_OPTIONS = (options?:QueueOptions) => {
+  const {durable = true, maxLength = 1000, messageTtl = ms("3m"), queueMode = "default", maxPriority, deadLetterExchange, deadLetterRoutingKey, expires} = options ?? {};
+  return {
+    durable, 
+    maxPriority: maxPriority ? priority[maxPriority] : priority["no-priority"],
+    arguments: {"x-queue-mode": queueMode},
+    messageTtl,
+    maxLength,
+    ...(deadLetterExchange && {deadLetterExchange}),
+    ...(deadLetterRoutingKey && {deadLetterRoutingKey}),
+    ...(expires && {expires}),
+
+  }
+}
+
 export type UserCreatedType = {
   type: "user-created";
   exchangeName: "user-events";
   queueName: `user-created-queue-${string}`;
   routingKey: "user-created";
+
+  // deadExchangeName: "dead-user-events";
+  // deadQueueName: `dead-user-created-queue-${string}`;
+  // deadRoutingKey: "dead-user-created";
+
+  // retryExchangeName?: "retry-user-events";
+  // retryQueueName?: `retry-user-created-queue-${string}`;
+  // retryRoutingKey?: "retry-user-created";
 };
 
 export type UserUpdatedType = {
