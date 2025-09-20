978986b8a789748010ba094a1573b677dd219144
xmarya <marya_hy@hotmail.com>
2025-09-20 10:38:36 +0300

    rabbitmq channel had been split into two, for the publisher and consumers

--- _config/rabbitmq/publishingChannel.ts
+++ _config/rabbitmq/publishingChannel.ts
@@ -3,23 +3,23 @@ import { Success } from "@Types/ResultTypes/Success";
 import amqp, {ConfirmChannel} from "amqplib";
 import { ms } from "ms";
 
-let channel:ConfirmChannel | null = null;
+let publishingChannel:ConfirmChannel | null = null;
 const isDevelopment = process.env.NODE_ENV === "development";
-export async function initialiseRabbitMQ() {
+export async function initialiseRabbitMQPublishingChannel() {
   const PORT = isDevelopment ? process.env.CLOUDAMQP_DEVELOPMENT_PORT : process.env.CLOUDAMQP_PRODUCTION_PORT;
   // const URL = `${process.env.CLOUDAMQP_URL}:${PORT}`
   try {
     const connection = await amqp.connect(process.env.CLOUDAMQP_URL);
-    channel = await connection.createConfirmChannel();
+    publishingChannel = await connection.createConfirmChannel();
     console.log("🐇 connected");
 } catch (error) {
     console.log("🐇🌋", error);
-    setTimeout(initialiseRabbitMQ, ms("2s"));
+    setTimeout(initialiseRabbitMQPublishingChannel, ms("2s"));
     new Failure("Failed to connect to RabbitMQ");
   }
 }
 
-export default function getRabbitChannel() {
-  if (!channel) return new Failure("rabbitChannel hasn't been initialised");
-  return new Success(channel);
+export default function getRabbitPublishingChannel() {
+  if (!publishingChannel) return new Failure("rabbitChannel hasn't been initialised");
+  return new Success(publishingChannel);
 }
