35f8b25f5366670426273a015fafc22c1fca1989
xmarya <marya_hy@hotmail.com>
2025-09-22 17:01:15 +0300

    deleteAllReviewsOfResource is now accepting StoreDeletedEvent

--- eventConsumers/deleteAllReviewsOfResource.ts
+++ eventConsumers/deleteAllReviewsOfResource.ts
@@ -1,14 +1,16 @@
 import { deleteAllResourceReviews } from "@repositories/review/reviewRepo";
 import { ProductDeletedEvent } from "@Types/events/ProductEvents";
+import { StoreDeletedEvent } from "@Types/events/StoreEvents";
 import { Failure } from "@Types/ResultTypes/errors/Failure";
 import { Success } from "@Types/ResultTypes/Success";
 import extractSafeThrowableResult from "@utils/extractSafeThrowableResult";
 import safeThrowable from "@utils/safeThrowable";
 import mongoose from "mongoose";
 
-async function deleteAllReviewsOfResource(event: ProductDeletedEvent /*TODO | StoreDeletedEvent*/) {
-  const { productId } = event.payload;
-  const resourceId = new mongoose.Types.ObjectId(productId);
+async function deleteAllReviewsOfResource(event: ProductDeletedEvent | StoreDeletedEvent) {
+  const { productId } = event.payload as ProductDeletedEvent["payload"];
+  const { storeId } = event.payload as StoreDeletedEvent["payload"];
+  const resourceId = productId ? new mongoose.Types.ObjectId(productId) : new mongoose.Types.ObjectId(storeId);
   const safeDeleteAllReviews = safeThrowable(
     () => deleteAllResourceReviews(resourceId),
     (error) => new Failure((error as Error).message)
