957c0f9a251b85f6658d78422c97f3f508f79142
xmarya <marya_hy@hotmail.com>
2025-09-22 08:57:44 +0300

    changed the updateProfileFactory implementation, new updateProfile service function was created + update users profile and confirmChangedEmail functions were integrated with outbox

---
+++ _services/_sharedServices/updateProfile.ts
@@ -0,0 +1,33 @@
+import { BadRequest } from "@Types/ResultTypes/errors/BadRequest";
+import { BaseUserData } from "@Types/Schema/Users/BasicUserTypes";
+import { NotAssistant } from "@Types/Schema/Users/NotAssistant";
+import updateProfileFactory from "./updateProfileFactory";
+import { startSession } from "mongoose";
+import createOutboxRecord from "./outboxRecordServices/createOutboxRecord";
+import { UserUpdatedEvent } from "@Types/events/UserEvents";
+
+async function updateProfile(user: NotAssistant, updatedData: Partial<BaseUserData>, emailConfirmed:boolean) {
+  const { userType, id } = user;
+  const { firstName, lastName } = updatedData;
+  if (firstName?.trim() === "" || lastName?.trim() === "") return new BadRequest("الرجاء تعبئة حقول الاسم بالكامل");
+
+  const updateProfileOfUserType = updateProfileFactory(userType);
+  const session = await startSession();
+
+  const updatedUserResult = await session.withTransaction(async()=> {
+    const updatedUserResult = await updateProfileOfUserType(id, updatedData, session);
+    if(updatedUserResult.ok) {
+      const payload:UserUpdatedEvent["payload"] = {
+        user: updatedUserResult.result,
+        emailConfirmed
+      }
+      await createOutboxRecord<[UserUpdatedEvent]>([{type: "user-updated", payload}], session);
+    }
+
+    return updatedUserResult
+  });
+
+  return updatedUserResult
+}
+
+export default updateProfile;
