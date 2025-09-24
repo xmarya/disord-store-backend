d942b4947451b221e8ca4f062f26ae3f590b901d
xmarya <marya_hy@hotmail.com>
2025-09-24 19:22:29 +0300

    completedOutboxRecordBullMQ was added to initiateBullMQJobs

--- loaders/bullmqJobs.ts
+++ loaders/bullmqJobs.ts
@@ -1,3 +1,4 @@
+import completedOutboxRecordBullMQ from "@externals/bullmq/jobProcessors/completedOutboxRecordsProcessor";
 import pendingOutboxRecordBullMQ from "@externals/bullmq/jobProcessors/getPendingOutboxRecordProcessor";
 import invoiceBullMQ from "@externals/bullmq/jobProcessors/invoiceProcessor";
 
@@ -5,6 +6,7 @@ import invoiceBullMQ from "@externals/bullmq/jobProcessors/invoiceProcessor";
 async function initiateBullMQJobs() {
   await invoiceBullMQ();
   await pendingOutboxRecordBullMQ();
+  await completedOutboxRecordBullMQ();
 }
 
 export default initiateBullMQJobs;
