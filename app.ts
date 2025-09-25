03139b8b6f01d9d28ce6046e11b99df4e31da47a
xmarya <marya_hy@hotmail.com>
2025-09-25 10:12:54 +0300

    commented out unwanted services in this branch

--- app.ts
+++ app.ts
@@ -1,20 +1,20 @@
 import { dbStartConnection } from "@config/db";
-import initiateBullMQJobs from "@loaders/bullmqJobs";
+// import initiateBullMQJobs from "@loaders/bullmqJobs";
 import expressLoader from "@loaders/express";
 import routerLoader from "@loaders/routers";
 import express from "express";
-import "@eventSubscribers/userEvents/initialiser";
-import "@eventSubscribers/productEvents/initialiser";
-import "@eventSubscribers/planEvents/planSubscriptionUpdate";
-import "eventSubscribers/queryResultsEvents/queryResultsFetched";
-import "@eventSubscribers/assistantEvents/initialiser";
+// import "@eventSubscribers/userEvents/initialiser";
+// import "@eventSubscribers/productEvents/initialiser";
+// import "@eventSubscribers/planEvents/planSubscriptionUpdate";
+// import "eventSubscribers/queryResultsEvents/queryResultsFetched";
+// import "@eventSubscribers/assistantEvents/initialiser";
 
 const app = express();
 async function startApp() {
-  await expressLoader(app);
-  await routerLoader(app);
+  expressLoader(app);
+  routerLoader(app);
   await dbStartConnection();
-  await initiateBullMQJobs();
+  // await initiateBullMQJobs();
 }
 
 await startApp();
