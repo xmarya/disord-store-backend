d57a3d3cd88fde87414d1576b72174e57495a90a
xmarya <marya_hy@hotmail.com>
2025-09-26 09:36:53 +0300

    replaced StoreDeletedEvent with StoreOwnerDeletedEvent

--- _services/auth/storeOwnerServices/deleteStoreOwnerAccount.ts
+++ _services/auth/storeOwnerServices/deleteStoreOwnerAccount.ts
@@ -1,40 +1,45 @@
-import { deleteStore } from "@repositories/store/storeRepo";
 import { deleteStoreOwner } from "@repositories/storeOwner/storeOwnerRepo";
 import createOutboxRecord from "@services/_sharedServices/outboxRecordServices/createOutboxRecord";
-import { StoreDeletedEvent } from "@Types/events/StoreEvents";
+import { StoreOwnerDeletedEvent } from "@Types/events/StoreOwnerEvents";
 import { UserDeletedEvent } from "@Types/events/UserEvents";
 import { Failure } from "@Types/ResultTypes/errors/Failure";
 import { Success } from "@Types/ResultTypes/Success";
 import { MongoId } from "@Types/Schema/MongoId";
 import mongoose, { startSession } from "mongoose";
 
-async function deleteStoreOwnerAccount({storeOwnerId, storeId}:{storeOwnerId: MongoId, storeId: MongoId}) {
+// this service function is to DELETE A STORE OWNER ACCOUNT BY THE OWNER OR THE ADMIN
+async function deleteStoreOwnerAccount({ storeOwnerId, storeId }: { storeOwnerId: MongoId; storeId: MongoId }) {
   const session = await startSession();
-  const {deletedStoreOwner, deletedStore} = await session.withTransaction(async () => {
-
+  const deletedStoreOwner = await session.withTransaction(async () => {
     const deletedStoreOwner = await deleteStoreOwner(storeOwnerId, session);
-    const deletedStore = await deleteStore(storeId, session);
 
-    if (deletedStoreOwner && deletedStore) {
-      const ownerPayload: UserDeletedEvent["payload"] = {
+    if (deletedStoreOwner) {
+      const userDeletedPayload: UserDeletedEvent["payload"] = {
         usersId: [deletedStoreOwner.id],
         emailsToDelete: [deletedStoreOwner.email],
         userType: deletedStoreOwner.userType,
       };
-      const storePayload:StoreDeletedEvent["payload"] = {
-        storeId: deletedStoreOwner.myStore || new mongoose.Types.ObjectId(storeId)
-      }
-      await createOutboxRecord<[UserDeletedEvent, StoreDeletedEvent]>([{ type: "user-deleted", payload:ownerPayload }, {type:"store-deleted", payload:storePayload}], session);
+      const ownerDeletedPayload: StoreOwnerDeletedEvent["payload"] = {
+        storeId: deletedStoreOwner.myStore || new mongoose.Types.ObjectId(storeId),
+        storeOwnerId: deletedStoreOwner._id as MongoId || new mongoose.Types.ObjectId(storeOwnerId)
+      };
+      await createOutboxRecord<[StoreOwnerDeletedEvent, UserDeletedEvent]>(
+        [
+          { type: "user-deleted", payload: userDeletedPayload },
+          { type: "storeOwner-deleted", payload: ownerDeletedPayload },
+        ],
+        session
+      );
     }
 
-    return {deletedStoreOwner, deletedStore};
+    return deletedStoreOwner;
   });
   await session.endSession();
 
   // ADD FEATURE for adding the deleted data to an AdminLog
 
   if (!deletedStoreOwner) return new Failure();
-  return new Success({deletedStoreOwner, deletedStore});
+  return new Success(deletedStoreOwner);
 }
 
 export default deleteStoreOwnerAccount;
