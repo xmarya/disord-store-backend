0945c7684ccf19f4fdb164996e510f3f1b624def
xmarya <marya_hy@hotmail.com>
2025-09-26 10:38:51 +0300

    fixed a bug cause by using wrong delete store function that also runs a separate transaction

--- _services/auth/storeOwnerServices/deleteMyStore.ts
+++ _services/auth/storeOwnerServices/deleteMyStore.ts
@@ -1,19 +1,22 @@
+import { deleteStore } from "@repositories/store/storeRepo";
 import { resetStoreOwnerToDefault } from "@repositories/storeOwner/storeOwnerRepo";
 import createOutboxRecord from "@services/_sharedServices/outboxRecordServices/createOutboxRecord";
 import deleteStorePermanently from "@services/auth/storeServices/deleteStorePermanently";
 import { StoreDeletedEvent } from "@Types/events/StoreEvents";
 import { UserUpdatedEvent } from "@Types/events/UserEvents";
+import { BadRequest } from "@Types/ResultTypes/errors/BadRequest";
 import { Failure } from "@Types/ResultTypes/errors/Failure";
 import { Success } from "@Types/ResultTypes/Success";
 import { MongoId } from "@Types/Schema/MongoId";
 import mongoose, { startSession } from "mongoose";
 
 async function deleteMyStore(storeId: MongoId) {
+  if(!storeId) return new BadRequest("لا يوجد لديك متجر لتتم عملية الحذف")
   const session = await startSession();
 
   const updatedOwner = await session.withTransaction(async () => {
     const updatedOwner = await resetStoreOwnerToDefault(storeId, session);
-    const deletedStore = await deleteStorePermanently(storeId, session);
+    const deletedStore = await deleteStore(storeId, session);
     if (updatedOwner && deletedStore) {
       const ownerPayload: UserUpdatedEvent["payload"] = {
         user: updatedOwner,
