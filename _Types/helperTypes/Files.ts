2ec8acc6dab203b63c050ce457d8120dd6b06f6f
xmarya <marya_hy@hotmail.com>
2025-10-08 13:40:15 +0300

    changes to Files type and its data structure

--- _Types/helperTypes/Files.ts
+++ _Types/helperTypes/Files.ts
@@ -1,32 +1,35 @@
 export type AcceptedImagesMIME = "image/png" | "image/jpeg" | "image/jpg" | "image/webp" | "image/svg+xml";
-export type AcceptedTextFilesMIME = "application/pdf";
+export type AcceptedDocumentsMIME = "application/pdf";
 
 export namespace MIME {
   export type Image = AcceptedImagesMIME;
-  export type TextFile = AcceptedTextFilesMIME;
-  export type Any = Image | TextFile;
+  export type Document = AcceptedDocumentsMIME;
+  export type Any = Image | Document;
 }
 
-export type FileTypes<T extends MIME.Any> = T extends MIME.TextFile ? "pdf" : "image" ;
+export type FileTypes<T extends MIME.Any> = T extends MIME.Document ? "document" : "image";
 
 export type ParsedFile = Record<string, Image | SVG | PDF>;
 
-export interface BaseFile<T extends MIME.Any> {
+interface BaseFile<T extends MIME.Any> {
   fileName: string;
   fileType: FileTypes<T>;
+  fileExtension: string;
   fileSizeInBytes: number;
   buffer: Buffer;
   mimeType: T;
   // svgContent?: string
 }
 
-export type FormattedFile<T extends MIME.Any> = {
-  fileType: FileTypes<T>;
-  fileName: string;
-  fileSize: number;
-  svgContent?: string;
-};
+// export type FormattedFile<T extends MIME.Any> = {
+//   fileType: FileTypes<T>;
+//   fileName: string;
+//   fileSize: number;
+//   svgContent?: string;
+// };
 
 interface Image extends BaseFile<Exclude<MIME.Image, "image/svg+xml">> {}
-interface SVG extends BaseFile<Extract<MIME.Image, "image/svg+xml">> { svgContent: string }
-interface PDF extends BaseFile<MIME.TextFile> {}
+interface SVG extends BaseFile<Extract<MIME.Image, "image/svg+xml">> {
+  svgContent: string;
+}
+interface PDF extends BaseFile<MIME.Document> {}
