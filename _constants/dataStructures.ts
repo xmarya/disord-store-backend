4193245c874d13e521069891afb538979deebba6
xmarya <marya_hy@hotmail.com>
2025-10-08 08:47:29 +0300

    edits to files data structure

--- _constants/dataStructures.ts
+++ _constants/dataStructures.ts
@@ -1,28 +1,26 @@
+import { AcceptedFilesMIME, AcceptedImagesMIME } from "@Types/helperTypes/Files";
+
 // for dates
 export const SUPPORTED_DATE_FORMATS = ["yyyy-MM-dd", "MM/dd/yyyy", "dd/MM/yyyy", "MMMM d, yyyy", "d MMMM, yyyy", "yyyy/MM/dd"];
 
 export const planTiers = {
-    "basic":1,
-    "plus":2,
-    "unlimited":3
+  basic: 1,
+  plus: 2,
+  unlimited: 3,
 };
 
 export const TokenSlicer = {
-    from:0,
-    to:12
-}
+  from: 0,
+  to: 12,
+};
 
+export const ACCEPTED_FILES_MIME:Array<AcceptedFilesMIME> = ["application/pdf"];
+export const ACCEPTED_IMAGE_MIME: Array<Exclude<AcceptedImagesMIME, "application/pdf">> = ["image/png", "image/jpeg", "image/jpg", "image/webp", "image/svg+xml"];
 
-export const ACCEPTED_FILES_TYPES = ["application/pdf"];
-export const ACCEPTED_IMAGE_TYPES = ["image/png", "image/jpeg", "image/jpg", "image/webp", "image/svg+xml"];
-
-
-export const FILES_SIGNATURES = {
+export const FILES_SIGNATURES_IN_HEX = {
   jpg: "FFD8",
   jpeg: "FFD8",
   png: "89504E470D0A1A0A",
   webp: "52494646",
-//   svg: "",
   pdf: "25504446",
-}; // in hex
-
+}; 
