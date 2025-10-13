631ed2a8531286ebc4b40468f07c78bae7cbc346
xmarya <marya_hy@hotmail.com>
2025-10-13 14:40:39 +0300

    upload file into cloudflare was completed and tested

---
+++ _services/_sharedServices/fileServices/uploadResourceFile.ts
@@ -0,0 +1,22 @@
+import cloudflareUpload from "@externals/cloudflare/uploadFile";
+import { ParsedFile, UploadFileData } from "@Types/helperTypes/Files";
+import { Success } from "@Types/ResultTypes/Success";
+
+async function uploadResourceFile(resourceDirectory: UploadFileData["resourceDirectory"], resourceId: string, parsedFile: Array<ParsedFile>) {
+  const uploadedFilesPaths: Array<Record<string, string>> = [];
+
+  for (const fileInfo of parsedFile) {
+    console.log("parsedFile.length", parsedFile.length);
+    const result = await cloudflareUpload({ resourceDirectory, resourceId, fileInfo });
+    if (!result.ok) return result;
+
+    const fieldName = fileInfo.streamName; // exp: avatar
+    const { result: fileUrl } = result; // exp: cdn.domain/filePath
+
+    uploadedFilesPaths.push({ [fieldName]: fileUrl });
+  }
+
+  return new Success(uploadedFilesPaths);
+}
+
+export default uploadResourceFile;
