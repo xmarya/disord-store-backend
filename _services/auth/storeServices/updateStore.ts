a4d81f8d0e82d571a193effd10a798ca85cf0f85
xmarya <marya_hy@hotmail.com>
2025-10-14 19:09:42 +0300

    removed all file logic from updateStore service function

--- _services/auth/storeServices/updateStore.ts
+++ _services/auth/storeServices/updateStore.ts
@@ -5,18 +5,11 @@ import { Failure } from "@Types/ResultTypes/errors/Failure";
 import { StoreDataBody } from "@Types/Schema/Store";
 import extractSafeThrowableResult from "@utils/extractSafeThrowableResult";
 import safeThrowable from "@utils/safeThrowable";
-import { ParsedFile } from "@Types/helperTypes/Files";
-import uploadFileAndMergeIntoBodyData from "@utils/files/uploadFilesAndMergeIntoBodyData";
 
-async function updateStore(storeId: MongoId, updatedData: Partial<StoreDataBody>, parsedFiles: Array<ParsedFile>) {
-
-  const mergedDataResult = await uploadFileAndMergeIntoBodyData("stores", storeId, parsedFiles, updatedData);
-
-  if (!mergedDataResult.ok) return mergedDataResult;
-  const { result: mergedData } = mergedDataResult;
+async function updateStore(storeId: MongoId, updatedData: Partial<StoreDataBody>) {
 
   const safeUpdateStore = safeThrowable(
-    () => updateDoc(Store, storeId, mergedData),
+    () => updateDoc(Store, storeId, updatedData),
     (error) => new Failure((error as Error).message)
   );
 
