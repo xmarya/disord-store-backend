f827fd1bd2ef199e336c862ce3556ebd20b52325
xmarya <marya_hy@hotmail.com>
2025-10-14 08:44:55 +0300

    getFilesList was created and tested

---
+++ externals/cloudflare/getFilesList.ts
@@ -0,0 +1,29 @@
+import { ListObjectsV2Command, ListObjectsV2CommandInput } from "@aws-sdk/client-s3";
+import s3 from "@config/S3Client";
+import { Failure } from "@Types/ResultTypes/errors/Failure";
+import { Success } from "@Types/ResultTypes/Success";
+import extractSafeThrowableResult from "@utils/extractSafeThrowableResult";
+import safeThrowable from "@utils/safeThrowable";
+
+async function getFilesList(filePath: string) {
+  const params: ListObjectsV2CommandInput = {
+    Bucket: process.env.CLOUDFLARE_BUCKET_NAME_DEVELOPMENT,
+    Prefix: filePath,
+  };
+
+  const command = new ListObjectsV2Command(params);
+  const commandResult = safeThrowable(
+    () => s3.send(command),
+    (error) => new Failure((error as Error).message)
+  );
+
+  const result = await extractSafeThrowableResult(() => commandResult);
+
+  if (!result.ok) return result;
+
+  const { Contents } = result.result;
+
+  return new Success(Contents);
+}
+
+export default getFilesList;
