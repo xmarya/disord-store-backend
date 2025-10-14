5dbf7eaa6d3020d90c88033490ac3d4f11106074
xmarya <marya_hy@hotmail.com>
2025-10-14 14:27:51 +0300

    changed the type of uploadResourceFile parameter

--- _services/_sharedServices/fileServices/uploadResourceFile.ts
+++ _services/_sharedServices/fileServices/uploadResourceFile.ts
@@ -1,13 +1,14 @@
 import cloudflareUpload from "@externals/cloudflare/uploadFile";
 import { ParsedFile, UploadFileData } from "@Types/helperTypes/Files";
 import { Success } from "@Types/ResultTypes/Success";
+import { MongoId } from "@Types/Schema/MongoId";
 
-async function uploadResourceFile(fileDirectory: UploadFileData["fileDirectory"], resourceId: string, parsedFile: Array<ParsedFile>) {
+async function uploadResourceFile(fileDirectory: UploadFileData["fileDirectory"], resourceId: MongoId, parsedFile: Array<ParsedFile>) {
   const uploadedFilesPaths: Array<Record<string, string>> = [];
 
   for (const fileInfo of parsedFile) {
     console.log("parsedFile.length", parsedFile.length);
-    const result = await cloudflareUpload({ fileDirectory, resourceId, fileInfo });
+    const result = await cloudflareUpload({ fileDirectory, resourceId:resourceId.toString(), fileInfo });
     if (!result.ok) return result;
 
     const fieldName = fileInfo.streamName; // exp: avatar
--- _utils/files/uploadFilesAndMergeIntoBodyData.ts
+++ _utils/files/uploadFilesAndMergeIntoBodyData.ts
@@ -7,7 +7,7 @@ import extractObjectsFromArray from "@utils/extractObjectsFromArray";
 async function uploadFilesAndMergeIntoBodyData<T>(resourceDirectory: UploadFileData["fileDirectory"], resourceId: MongoId, parsedFile: Array<ParsedFile>, bodyData: T) {
   if(!parsedFile.length) return new Success(bodyData);
 
-  const uploadResult = await uploadResourceFile(resourceDirectory, resourceId.toString(), parsedFile);
+  const uploadResult = await uploadResourceFile(resourceDirectory, resourceId, parsedFile);
   if (!uploadResult.ok) return uploadResult;
 
   const { result: uploadedFilesPaths } = uploadResult;
