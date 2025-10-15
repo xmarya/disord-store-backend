502264cb8d3f96264d69d4668ab489ef9aacf5da
xmarya <marya_hy@hotmail.com>
2025-10-15 18:52:47 +0300

    ProductCreatedEvent was added to product events

--- _Types/events/ProductEvents.ts
+++ _Types/events/ProductEvents.ts
@@ -1,7 +1,14 @@
 import { MongoId } from "@Types/Schema/MongoId";
 import { DomainEvent } from "./DomainEvent";
 import { OutboxEvent } from "./OutboxEvents";
+import { ProductDocument } from "@Types/Schema/Product";
 
+export interface ProductCreatedEvent extends OutboxEvent {
+  type: "product-created",
+  payload: {
+    product:ProductDocument
+  }
+}
 export interface ProductUpdatedEvent extends DomainEvent {
   type: "product-updated";
   payload: {
