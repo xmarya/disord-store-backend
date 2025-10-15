934e38a28656996fe4ee294de8476e80a2d6dae2
xmarya <marya_hy@hotmail.com>
2025-10-15 13:58:50 +0300

    enhanced the return type of extractDataFromFileUrl

--- _utils/files/extractDataFromFileUrl.ts
+++ _utils/files/extractDataFromFileUrl.ts
@@ -1,4 +1,5 @@
 import getCloudflareConfig from "@externals/cloudflare/getCloudflareConfig";
+import { UploadFileData } from "@Types/helperTypes/Files";
 
 const extractDataFromFileUrl = (fileUrl: string) => {
   const { publicDomain } = getCloudflareConfig();
@@ -6,7 +7,7 @@ const extractDataFromFileUrl = (fileUrl: string) => {
   const filePath = fileUrl.replace(publicDomain as string, "");
   const [fileDirectory, resourceId, file] = filePath.split("/"); // "users/123456789/avatar.jpeg"
 
-  return { filePath, fileDirectory, resourceId, file };
+  return { filePath, fileDirectory, resourceId, file } as {filePath:string, fileDirectory:UploadFileData["fileDirectory"], resourceId:string, file:string};
 };
 
 export default extractDataFromFileUrl;
