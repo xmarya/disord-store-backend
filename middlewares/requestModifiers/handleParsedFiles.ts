f0eeb6330048a65b8c7de626679da3681afdf12e
xmarya <marya_hy@hotmail.com>
2025-10-15 16:45:50 +0300

    handleParsedFiles middleware was created

---
+++ middlewares/requestModifiers/handleParsedFiles.ts
@@ -0,0 +1,20 @@
+import { UploadFileData } from "@Types/helperTypes/Files";
+import { catchAsync } from "@utils/catchAsync";
+import uploadFilesAndMergeIntoBodyData from "@utils/files/uploadFilesAndMergeIntoBodyData";
+import returnError from "@utils/returnError";
+import { format } from "date-fns";
+
+const handleParsedFiles = (fileDirectory: UploadFileData["fileDirectory"]) => catchAsync(async (request, response, next) => {
+    if (!request.parsedFile.length) return next();
+
+    const id = Object.values(request.params)[0] ?? format(new Date(), "yyMMdd-HHmmssSSS");
+    const result = await uploadFilesAndMergeIntoBodyData(fileDirectory, id, request.parsedFile, request.body);
+
+    if (!result.ok) return next(returnError(result));
+    const { result: body } = result;
+
+    request.body = body;
+    return next();
+  });
+
+export default handleParsedFiles;
