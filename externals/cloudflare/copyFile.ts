43d7b2e48341ef03ef2970b607931b10a7fbdec8
xmarya <marya_hy@hotmail.com>
2025-10-15 15:33:37 +0300

    copyFile function was created

---
+++ externals/cloudflare/copyFile.ts
@@ -0,0 +1,25 @@
+import { CopyObjectCommand, CopyObjectCommandInput } from "@aws-sdk/client-s3";
+import getCloudflareConfig from "./getCloudflareConfig";
+import safeThrowable from "@utils/safeThrowable";
+import { Failure } from "@Types/ResultTypes/errors/Failure";
+import s3 from "@config/S3Client";
+import extractSafeThrowableResult from "@utils/extractSafeThrowableResult";
+
+async function copyFile({ oldPath, newPath }: { oldPath: string; newPath: string }) {
+  const { bucketName } = getCloudflareConfig();
+  const params: CopyObjectCommandInput = {
+    Bucket: bucketName,
+    CopySource: `/${bucketName}/${oldPath}`,
+    Key: newPath,
+  };
+  const command = new CopyObjectCommand(params);
+
+  const result = safeThrowable(
+    () => s3.send(command),
+    (error) => new Failure((error as Error).message)
+  );
+
+  return await extractSafeThrowableResult(() => result);
+}
+
+export default copyFile;
