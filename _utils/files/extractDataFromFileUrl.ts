50466e29207b93719678539f619dc8db7deb9ff7
xmarya <marya_hy@hotmail.com>
2025-10-15 13:44:51 +0300

    moved extractDataFromFileUrl file into the correct location

--- _utils/files/extractDataFromFileUrl.ts
+++ _utils/files/extractDataFromFileUrl.ts
@@ -1,4 +1,8 @@
-const extractDataFromFileUrl = ({ publicDomain, fileUrl }: { publicDomain: string; fileUrl: string }) => {
+import getCloudflareConfig from "@externals/cloudflare/getCloudflareConfig";
+
+const extractDataFromFileUrl = (fileUrl: string) => {
+  const { publicDomain } = getCloudflareConfig();
+
   const filePath = fileUrl.replace(publicDomain as string, "");
   const [fileDirectory, resourceId, file] = filePath.split("/"); // "users/123456789/avatar.jpeg"
 
