271c9b56923cdbf54632a6238b8417518f9010d2
xmarya <marya_hy@hotmail.com>
2025-10-15 16:38:24 +0300

    removed the step where the newly created store is assigned to the storeOwner to be a consumer of StoreCreatedEvent

--- _services/auth/storeServices/createNewStore.ts
+++ _services/auth/storeServices/createNewStore.ts
@@ -1,41 +1,28 @@
 import { createStore } from "@repositories/store/storeRepo";
-import { assignStoreToOwner } from "@repositories/storeOwner/storeOwnerRepo";
 import createOutboxRecord from "@services/_sharedServices/outboxRecordServices/createOutboxRecord";
-import { UserUpdatedEvent } from "@Types/events/UserEvents";
+import { StoreCreatedEvent } from "@Types/events/StoreEvents";
 import { Failure } from "@Types/ResultTypes/errors/Failure";
 import { Success } from "@Types/ResultTypes/Success";
 import { FullStoreDataBody, StoreDataBody } from "@Types/Schema/Store";
-import { AllUsers } from "@Types/Schema/Users/AllUser";
 import { StoreOwnerDocument } from "@Types/Schema/Users/StoreOwner";
 import { startSession } from "mongoose";
 
-async function createNewStore(storeOwner: StoreOwnerDocument, storeData: StoreDataBody, emailConfirmed: boolean) {
-  const { storeName, description,productsType, logo } = storeData;
-  //TODO: handling logo and uploading it to cloudflare
-  const data: FullStoreDataBody = { storeName, productsType, description, logo, owner: storeOwner.id, inPlan: storeOwner.subscribedPlanDetails.planName };
+async function createNewStore(storeOwner: StoreOwnerDocument, storeData: StoreDataBody) {
+
+  const { storeName, description, productsType } = storeData;
+  const data: FullStoreDataBody = { storeName, productsType, description, owner: storeOwner.id, inPlan: storeOwner.subscribedPlanDetails.planName };
+  
   const session = await startSession();
+  const newStore = await session.withTransaction(async () => {
 
-  const { newStore, updatedOwner } = await session.withTransaction(async () => {
     const newStore = await createStore(data, session);
-    const updatedOwner = await assignStoreToOwner(data.owner, newStore.id, session);
+    if (newStore.id) await createOutboxRecord<[StoreCreatedEvent]>([{ type: "store-created", payload: { store: newStore } }], session);
 
-    await createOutboxRecord<[UserUpdatedEvent]>(
-      [
-        {
-          type: "user-updated",
-          payload: {
-            user: updatedOwner as AllUsers,
-            emailConfirmed,
-          },
-        },
-      ],
-      session
-    );
-    return { newStore, updatedOwner };
+    return newStore;
   });
   await session.endSession();
 
-  if (!newStore.id || !updatedOwner?.myStore) return new Failure("حدثت مشكلة أثناء انشاء متجرك");
+  if (!newStore.id) return new Failure("حدثت مشكلة أثناء انشاء متجرك");
 
   return new Success(newStore);
 }
