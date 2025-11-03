d903f21045670ab949ed2457b41ef0d006306769
xmarya <marya_hy@hotmail.com>
2025-11-03 13:55:41 +0300

    decided to revert to the old logic where the store is created and the storeOwner is updated in the same service function

--- _services/auth/storeServices/createNewStore.ts
+++ _services/auth/storeServices/createNewStore.ts
@@ -1,8 +1,11 @@
 import { createStore } from "@repositories/store/storeRepo";
+import { assignStoreToOwner } from "@repositories/storeOwner/storeOwnerRepo";
 import createOutboxRecord from "@services/_sharedServices/outboxRecordServices/createOutboxRecord";
 import { StoreCreatedEvent } from "@Types/events/StoreEvents";
+import { UserUpdatedEvent } from "@Types/events/UserEvents";
 import { Failure } from "@Types/ResultTypes/errors/Failure";
 import { Success } from "@Types/ResultTypes/Success";
+import { MongoId } from "@Types/Schema/MongoId";
 import { FullStoreDataBody, StoreDataBody } from "@Types/Schema/Store";
 import { StoreOwnerDocument } from "@Types/Schema/Users/StoreOwner";
 import { startSession } from "mongoose";
@@ -16,7 +19,13 @@ async function createNewStore(storeOwner: StoreOwnerDocument, storeData: StoreDa
   const newStore = await session.withTransaction(async () => {
 
     const newStore = await createStore(data, session);
-    if (newStore.id) await createOutboxRecord<[StoreCreatedEvent]>([{ type: "store-created", payload: { store: newStore } }], session);
+    const updatedOwner = await assignStoreToOwner(storeOwner._id as MongoId, newStore._id as MongoId, session);
+    if (newStore?.id && updatedOwner?.id) {
+      await createOutboxRecord<[StoreCreatedEvent, UserUpdatedEvent]>([
+        { type: "store-created", payload: { store: newStore } },
+        {type :"user-updated", payload: { user:updatedOwner}}
+      ], session);
+    }
 
     return newStore;
   });
