ca4747c692bdcfa2d51eaf8ae1cd642acf1ee8d1
xmarya <marya_hy@hotmail.com>
2025-11-15 08:45:30 +0300

    moved createNewInvoiceConsumer from invoice folder to order folsdr as this consumer is gonna consume the OrderCreated event, not the InvoiceCreated event

--- eventConsumers/order/createNewInvoiceConsumer.ts
+++ eventConsumers/order/createNewInvoiceConsumer.ts
@@ -1,9 +1,11 @@
+import createOutboxRecord from "@services/_sharedServices/outboxRecordServices/createOutboxRecord";
 import createNewInvoice from "@services/auth/invoiceServices/createNewInvoice";
-import updateStoreStats from "@services/auth/storeServices/storeStats/updateStoreStats";
+import { InvoiceCreated } from "@Types/events/InvoiceEvents";
 import { OrderCreated } from "@Types/events/OrderEvents";
 import { Failure } from "@Types/ResultTypes/errors/Failure";
 import { Success } from "@Types/ResultTypes/Success";
 import { format } from "date-fns";
+import { startSession } from "mongoose";
 
 async function createNewInvoiceConsumer(event: OrderCreated) {
   const {
@@ -18,8 +20,16 @@ async function createNewInvoiceConsumer(event: OrderCreated) {
   const invoiceId = format(releasedAt, "yyMMdd-HHmmssSSS");
 
   const fullData = { releasedAt, invoiceId, ...event.payload.invoice };
-  await updateStoreStats(fullData, operationType);
-  const newInvoice = await createNewInvoice(fullData);
+
+  const session = await startSession();
+
+  const newInvoice = await session.withTransaction(async() => {
+    const newInvoice = await createNewInvoice(fullData);
+    if(newInvoice) await createOutboxRecord<[InvoiceCreated]>([{type: "invoice-created", payload:{storesStats:fullData, operationType}}], session);
+    return newInvoice;
+  });
+
+  await session.endSession();
 
   if(newInvoice.ok) return new Success({ serviceName: "invoicesCollection", ack: true });
   return new Failure(newInvoice.message, { serviceName: "invoicesCollection", ack: false });
