90e6207d9451f47c9ea352284fb1c25b383ce0c4
xmarya <marya_hy@hotmail.com>
2025-11-16 14:29:23 +0300

    ReviewCreated event was created

--- _Types/events/ReviewEvents.ts
+++ _Types/events/ReviewEvents.ts
@@ -1,7 +1,13 @@
-import { MongoId } from "@Types/Schema/MongoId";
 import { OutboxEvent } from "./OutboxEvents";
 import { ReviewDocument } from "@Types/Schema/Review";
 
+export interface ReviewCreated extends OutboxEvent {
+  type: "review-created";
+  outboxRecordId: string;
+  payload: {
+    review: ReviewDocument;
+  };
+}
 export interface StoreRepliedToUserReview extends OutboxEvent {
   type: "store-replied-to-review";
   outboxRecordId: string;
