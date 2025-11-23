a1a29dbb151080fc92d49a14cf2def2d25269804
xmarya <marya_hy@hotmail.com>
2025-11-23 19:10:15 +0300

    setResourceRating is now a consumer

--- _services/auth/resourcesReviewServices/setResourceRating.ts
+++ _services/auth/resourcesReviewServices/setResourceRating.ts
@@ -1,22 +1,24 @@
-import { INTERNAL_ERROR_MESSAGE } from "@constants/primitives";
 import { setRanking } from "@repositories/ranking/rankingRepo";
 import { calculateRatingsAverage } from "@repositories/review/reviewRepo";
-import { Model } from "@Types/Schema/Model";
-import { MongoId } from "@Types/Schema/MongoId";
+import { ReviewCreated } from "@Types/events/ReviewEvents";
+import { Failure } from "@Types/ResultTypes/errors/Failure";
+import { Success } from "@Types/ResultTypes/Success";
 import { startSession } from "mongoose";
-import { err } from "neverthrow";
 
-async function setResourceRating(Model: Extract<Model, "Store" | "Product">, resourceId: MongoId) {
+async function setResourceRating(event: ReviewCreated) {
+  const { storeOrProduct, reviewedResourceId } = event.payload.review;
   const session = await startSession();
 
   try {
     session.startTransaction();
-    await calculateRatingsAverage(Model, resourceId, session);
-    await setRanking(Model, session);
+    await calculateRatingsAverage(storeOrProduct, reviewedResourceId, session);
+    await setRanking(storeOrProduct, session);
     await session.commitTransaction();
+
+    return new Success({ serviceName: "setRanking", ack: true });
   } catch (error) {
     await session.abortTransaction();
-    err(INTERNAL_ERROR_MESSAGE);
+    return new Failure((error as Error).message, { serviceName: "setRanking", ack: true });
   } finally {
     await session.endSession();
   }
--- externals/rabbitmq/reviewCreated/reviewCreatedConsumers.ts
+++ externals/rabbitmq/reviewCreated/reviewCreatedConsumers.ts
@@ -2,23 +2,36 @@ import { ConsumerRegister, ReviewCreatedType } from "@Types/events/OutboxEvents"
 import { ReviewCreated } from "@Types/events/ReviewEvents";
 import newReviewNotificationConsumer from "eventConsumers/review/newReviewConsumer";
 import reviewCreatedRegister from "./reviewCreatedRegister";
+import setResourceRating from "@services/auth/resourcesReviewServices/setResourceRating";
 
 const consumers = {
-    novu: {
-        receiver:newReviewNotificationConsumer,
-        queueName:"review-created-queue-novu",
-        queueOptions: {queueMode:"lazy", maxPriority:"hight", durable:true},
-        retryLetterOptions: {
-            mainExchangeName:"main-review-events",
-            mainRoutingKey:"review-created",
-            deadExchangeName:"dead-review-events",
-            deadRoutingKey:"dead-review-created",
-            deadQueueName:"dead-review-created-queue-novu"
-
-        }
-    }
+  novu: {
+    receiver: newReviewNotificationConsumer,
+    queueName: "review-created-queue-novu",
+    queueOptions: { queueMode: "lazy", maxPriority: "hight", durable: true },
+    retryLetterOptions: {
+      mainExchangeName: "main-review-events",
+      mainRoutingKey: "review-created",
+      deadExchangeName: "dead-review-events",
+      deadRoutingKey: "dead-review-created",
+      deadQueueName: "dead-review-created-queue-novu",
+    },
+  },
+  setRanking: {
+    receiver:setResourceRating,
+    queueName: "review-created-queue-setRanking",
+    queueOptions: { queueMode: "lazy", maxPriority: "hight", durable: true },
+    retryLetterOptions: {
+      mainExchangeName: "main-review-events",
+      mainRoutingKey: "review-created",
+      deadExchangeName: "dead-review-events",
+      deadRoutingKey: "dead-review-created",
+      deadQueueName: "dead-review-created-queue-setRanking",
+    },
+  },
 } satisfies Record<string, ConsumerRegister<ReviewCreatedType, ReviewCreated>>;
 
 export default function reviewCreatedConsumers() {
   reviewCreatedRegister(consumers["novu"]);
+  reviewCreatedRegister(consumers["setRanking"]);
 }
