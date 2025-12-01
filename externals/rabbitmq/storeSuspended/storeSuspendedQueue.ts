b26f7cc64f58f65e784ec729717a5b7bb2004bd9
xmarya <marya_hy@hotmail.com>
2025-12-01 17:06:43 +0300

    storeSuspendedQueue was created

---
+++ externals/rabbitmq/storeSuspended/storeSuspendedQueue.ts
@@ -0,0 +1,27 @@
+import getRabbitConsumingChannel from "@config/rabbitmq/consumingChannel";
+import { QUEUE_OPTIONS } from "@constants/rabbitmq";
+import { QueueOptions, StoreSuspendedType } from "@Types/events/OutboxEvents";
+import { Failure } from "@Types/ResultTypes/errors/Failure";
+import { Success } from "@Types/ResultTypes/Success";
+
+const exchangeName: StoreSuspendedType["exchangeName"] = "main-store-events";
+const routingKey: StoreSuspendedType["routingKey"] = "store-suspended";
+
+async function storeSuspendedQueue(queueName: StoreSuspendedType["queueName"], queueOptions?: QueueOptions) {
+  const result = getRabbitConsumingChannel();
+  if (!result.ok) return new Failure(result.message);
+
+  const { result: channel } = result;
+  const options = QUEUE_OPTIONS(queueOptions);
+  try {
+    await channel.assertExchange(exchangeName, "direct", { durable: true });
+    await channel.assertQueue(queueName, options);
+    await channel.bindQueue(queueName, exchangeName, routingKey);
+
+    return new Success({ channel });
+  } catch (error) {
+    return new Failure((error as Error).message);
+  }
+}
+
+export default storeSuspendedQueue;
