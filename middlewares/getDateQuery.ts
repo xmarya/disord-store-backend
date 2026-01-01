c5421ba57dcc35938c8cae50201279fd718e4212
xmarya <marya_hy@hotmail.com>
2026-01-01 11:50:20 +0300

    changed the logic of getting and formatting of getDateQuery middleware

--- middlewares/getDateQuery.ts
+++ middlewares/getDateQuery.ts
@@ -5,13 +5,13 @@ import { buildDateFilter } from "@utils/queryModifiers/formatDateQuery";
 import { SUPPORTED_DATE_FORMATS } from "../_constants/dataStructures";
 
 const getDateQuery = catchAsync(async (request, response, next) => {
-  const { dates, sortBy, sortOrder } = request.body;
+  const { dateFrom, dateTo, sortBy, sortOrder } = request.query as { dateFrom: string; dateTo: string; sortBy: string; sortOrder: "desc" | "asc" };
 
-  // const filter:string[] = dates?.length > 0 ? dates : [
-  //     lightFormat(startOfMonth(now), "yyyy-MM-dd"),
-  //     lightFormat(endOfMonth(now), "yyyy-MM-dd")
-  // ]
-  const filter: string[] = dates?.length > 0 ? dates : [lightFormat(new Date(), "yyyy-MM-dd")]; // set today as the default value if there is no dates array
+  const filter: string[] = [dateFrom, dateTo].map((date) => {
+    if (!date || date.toString().trim() === "") return lightFormat(new Date(), "yyyy-MM-dd");
+    return date.toString();
+  });
+
   const dateFilter = buildDateFilter(filter);
   if (!dateFilter) return next(new AppError(400, `Please provide a date with one of these format: ${SUPPORTED_DATE_FORMATS.join(", ")}`));
 
